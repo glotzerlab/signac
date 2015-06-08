4ba19ec1388dc158bb3b9658acacc457a16a4f81
Carl Simon Adorf <csadorf@umich.edu>
2015-06-08 09:20:23 -0400

    Logging is error tolerant and supports pymongo3.

--- src/compdb/contrib/logging.py
+++ src/compdb/contrib/logging.py
@@ -3,6 +3,9 @@ from . concurrency import DocumentLock
 
 DOCUMENT_LOCK_ID = 0
 
+import pymongo
+PYMONGO_3 = pymongo.version_tuple[0] == 3
+
 class MongoDBHandler(logging.Handler):
 
     def __init__(self, collection, lock_id = None):
@@ -15,10 +18,23 @@ class MongoDBHandler(logging.Handler):
         super().__init__()
     
     def _generate_doc(self, record):
-        return record.__dict__
+        return dict(record.__dict__)
 
     def emit(self, record):
-        self._collection.insert(self._generate_doc(record))
+        try:
+            return self._emit(record)
+        except Exception as error:
+            msg = "Error during attempt to log '{record}': {error}."
+            print(msg.format(record=record, error=error))
+            return False
+
+    def _emit(self, record):
+        record_doc = self._generate_doc(record)
+        assert not '_id' in record_doc
+        if PYMONGO_3:
+            self._collection.insert_one(record_doc)
+        else:
+            self._collection.insert(record_doc)
 
     def lock(self):
         return DocumentLock(
