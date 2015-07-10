06e284e014bbb5d5aa0e8c43f1d3ec15a9a73b8d
Carl Simon Adorf <csadorf@umich.edu>
2015-07-10 11:52:30 -0400

    Implement MongoDBSet.

---
+++ src/compdb/core/mongodb_set.py
@@ -0,0 +1,89 @@
+import logging
+logger = logging.getLogger(__name__)
+
+from .mongodb_queue import MongoDBQueue
+
+import jsonpickle, json
+import hashlib
+
+KEY_CHECKSUM = 'checksum'
+KEY_ELEMENT = 'element'
+
+def encode_element(element):
+    binary = json.dumps(json.loads(jsonpickle.dumps(element)), sort_keys=True).encode()
+    return binary
+
+def decode_element(binary):    
+    return jsonpickle.loads(binary.decode())
+
+def calculate_checksum(binary):
+    m = hashlib.sha1()
+    m.update(binary)
+    return m.hexdigest()
+
+class MongoDBSet(object):
+    
+    def __init__(self, collection):
+        """Initialize an instance of MongoDBSet.
+
+        :param collection: The MongoDB collection to be used for this set.
+        """
+        self._collection = collection
+
+    def __len__(self):
+        """Return the size of the set."""
+        return self._collection.find().count()
+
+    def _filter(self, elem):
+        return {'_id': calculate_checksum(encode_element(elem))}
+
+    def __contains__(self, elem):
+        """Returns true if elem is a member of the set."""
+        return self._collection.find_one(self._filter(elem)) is not None
+    
+    def add(self, elem):
+        """Add element to the set, if not already present."""
+        from pymongo.errors import DuplicateKeyError
+        binary = encode_element(elem)
+        try:
+            decoded = decode_element(binary)
+            if elem != decoded:
+                raise ValueError((elem, decoded))
+        except Exception as error:
+            msg = "Attempt to encode element '{}' failed: {}."
+            raise ValueError(msg.format(elem, error))
+        checksum = calculate_checksum(binary)
+        doc = {
+            '_id': checksum,
+            KEY_ELEMENT : binary}
+        try:    
+            result = self._collection.insert_one(doc)
+        except DuplicateKeyError:
+            pass
+
+    def remove(self, elem):
+        """Remove element from the set.
+        
+        :raises KeyError if elem is not member of the set."""
+        result = self._collection.delete_one(self._filter(elem))
+        if not result.deleted_count:
+            raise KeyError(elem)
+
+    def discard(self, elem):
+        """Remove element from the set if it is present."""
+        self._collection.delete_one(self._filter(elem))
+
+    def pop(self):
+        """Remove and return an arbitrary element from the set.
+
+        :raises KeyError if the set is empty.
+        """
+        document = self._collection.find_one_and_delete({})
+        if document is None:
+            raise KeyError()
+        else:
+            return decode_element(document[KEY_ELEMENT])
+
+    def clear(self):
+        """Remove all elements from the set."""
+        self._collection.delete_many({})
---
+++ tests/test_mongodbset.py
@@ -0,0 +1,147 @@
+import unittest
+from contextlib import contextmanager
+
+from compdb.core.mongodb_set import MongoDBSet
+
+import pymongo
+PYMONGO_3 = pymongo.version_tuple[0] == 3
+
+import warnings
+warnings.simplefilter('default')
+warnings.filterwarnings('error', category=DeprecationWarning, module='compdb')
+
+def testdata():
+    import uuid
+    return str(uuid.uuid4())
+
+def get_item():
+    return {'my_item': testdata()}
+
+def get_collection_handle(_id):
+    from compdb.core.config import load_config
+    from pymongo import MongoClient
+    config = load_config()
+    client = MongoClient(config['database_host'])
+    db = client['testing']
+    if _id is None:
+        _id = uuid.uuid4()
+    collection_name = 'test_mongodb_mongoset_{}'.format(_id)
+    return db[collection_name]
+
+@contextmanager
+def get_collection(_id = None):
+    if _id is None:
+        import uuid
+        _id = uuid.uuid4()
+    collection = get_collection_handle(_id)
+    try:
+        yield collection
+    except Exception:
+        raise
+    finally:
+        collection.drop()
+
+@contextmanager
+def get_mongoset(id_set = None):
+    with get_collection(id_set) as collection:
+        yield MongoDBSet(collection)
+
+class MongoDBSetTest(unittest.TestCase):
+
+    def test_init(self):
+        with get_collection() as collection:
+            mongoset = MongoDBSet(collection)
+
+    def test_add_and_pop(self):
+        item = get_item()
+        with get_mongoset() as mongoset:
+            mongoset.add(item)
+            item2 = mongoset.pop()
+            self.assertEqual(item, item2)
+            with self.assertRaises(KeyError):
+                mongoset.pop()
+
+    def test_add_and_pop_multiple(self):
+        item1 = get_item()
+        item2 = get_item()
+        with get_mongoset() as mongoset:
+            mongoset.add(item1)
+            mongoset.add(item1)
+            mongoset.add(item2)
+            mongoset.add(item2)
+            c_item1 = mongoset.pop()
+            c_item2 = mongoset.pop()
+            self.assertIn(c_item1, (item1, item2))
+            self.assertIn(c_item2, (item1, item2))
+            with self.assertRaises(KeyError):
+                mongoset.pop()
+    
+    def test_contains(self):
+        item1 = get_item()
+        item2 = get_item()
+        with get_mongoset() as mongoset:
+            self.assertNotIn(item1, mongoset)
+            self.assertNotIn(item2, mongoset)
+            mongoset.add(item1)
+            self.assertIn(item1, mongoset)
+            self.assertNotIn(item2, mongoset)
+            mongoset.add(item2)
+            self.assertIn(item1, mongoset)
+            self.assertIn(item2, mongoset)
+            r_item1 = mongoset.pop()
+            self.assertEqual(item1, r_item1)
+            self.assertNotIn(item1, mongoset)
+            self.assertIn(item2, mongoset)
+            r_item2 = mongoset.pop()
+            self.assertEqual(item2, r_item2)
+            self.assertNotIn(item1, mongoset)
+            self.assertNotIn(item2, mongoset)
+
+    def test_remove(self):
+        item1 = get_item()
+        item2 = get_item()
+        with get_mongoset() as mongoset:
+            self.assertNotIn(item1, mongoset)
+            self.assertNotIn(item2, mongoset)
+            mongoset.add(item1)
+            self.assertIn(item1, mongoset)
+            with self.assertRaises(KeyError):
+                mongoset.remove(item2)
+            mongoset.discard(item2)
+            mongoset.remove(item1)
+            self.assertNotIn(item1, mongoset)
+            mongoset.add(item2)
+            self.assertIn(item2, mongoset)
+            mongoset.discard(item2)
+            self.assertNotIn(item2, mongoset)
+
+    def test_clear(self):
+        item1 = get_item()
+        item2 = get_item()
+        with get_mongoset() as mongoset:
+            self.assertNotIn(item1, mongoset)
+            self.assertNotIn(item2, mongoset)
+            mongoset.add(item1)
+            mongoset.add(item2)
+            self.assertIn(item1, mongoset)
+            self.assertIn(item2, mongoset)
+            mongoset.clear()
+            self.assertNotIn(item1, mongoset)
+            self.assertNotIn(item2, mongoset)
+
+    def test_len(self):
+        item1 = get_item()
+        item2 = get_item()
+        with get_mongoset() as mongoset:
+            self.assertEqual(0, len(mongoset))
+            mongoset.add(item1)
+            self.assertEqual(1, len(mongoset))
+            mongoset.add(item2)
+            self.assertEqual(2, len(mongoset))
+            mongoset.remove(item1)
+            self.assertEqual(1, len(mongoset))
+            mongoset.discard(item2)
+            self.assertEqual(0, len(mongoset))
+
+if __name__ == '__main__':
+    unittest.main()
