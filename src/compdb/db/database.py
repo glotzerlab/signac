3ca73997b6409260124f8808181f5c31f21ca02f
Carl Simon Adorf <csadorf@umich.edu>
2015-07-16 00:00:17 -0400

    Add docstrings to the database API.

--- src/compdb/db/database.py
+++ src/compdb/db/database.py
@@ -1,3 +1,10 @@
+"""Main module for the Database implementation.
+
+This module contains the implementation of the CompMatDB API.
+
+See also: https://bitbucket.org/glotzer/compdb/wiki/latest/compmatdb
+"""
+
 import logging
 import copy
 import uuid
@@ -35,6 +42,7 @@ KEY_DOC_DATA = 'data'
 ILLEGAL_AGGREGATION_KEYS = ['$group', '$out']
 
 def hash_module(c):
+    "Calculate hash value for the module of c."
     module = inspect.getmodule(c)
     src_file = inspect.getsourcefile(module)
     m = hashlib.md5()
@@ -43,17 +51,23 @@ def hash_module(c):
     return m.hexdigest()
 
 def hash_source(c):
+    "Calculate hash value for the source code of c."
     m = hashlib.md5()
     m.update(inspect.getsource(c).encode())
     return m.hexdigest()
 
 def callable_name(c):
+    "Return a unique name for c."
     try:
         return c.__name__
     except AttributeError:
         return c.name()
 
 def callable_spec(c):
+    """Generate a mappable, which can be used as filter for c.
+
+    This function is primarily used to query cached result values for c.
+    """
     assert callable(c)
     try:
         spec = {
@@ -69,11 +83,13 @@ def callable_spec(c):
     return spec
 
 def encode(data):
+    "Encode data in JSON."
     binary = jsonpickle.encode(data).encode()
     #binary = json.dumps(data).encode()
     return binary
 
 def decode(data):
+    "Decode data, which is expected to be in JSON format."
     data = jsonpickle.decode(data.decode())
     if isinstance(data, dict):
         if 'py/object' in data:
@@ -83,6 +99,9 @@ def decode(data):
     return data
 
 def generate_auto_network():
+    """Generate a formats network from all registered formats and adapters.
+    Every adapter in the global namespace is automatically registered.
+    """
     network = nx.DiGraph()
     network.add_nodes_from(formats.BASICS)
     network.add_nodes_from(conversion.BasicFormat.registry.values())
@@ -93,9 +112,15 @@ def generate_auto_network():
     return network
 
 class UnsupportedExpressionError(ValueError):
+    """This exception is raised when a legal MongoDB expression is not supported."""
     pass
 
 class FileCursor(object):
+    """Iterator over database result cursors.
+    
+    This class should not be instantiated by developers directly.
+    See find() and aggregate() instead.
+    """
 
     def __init__(self, db, call_dict, projection = None):
         self._db = db
@@ -114,8 +139,20 @@ class FileCursor(object):
         return {}
 
 class Database(object):
+"""The CompMatDB base class.
+
+This object provides the CompMatDB API.
+"""
 
     def __init__(self, db, config = None):
+        """Create a new Database object.
+
+        :param db: The MongoDB backend database object.
+        :param config: A compdb config object.
+
+        This function should not be called directly.
+        See compdb.db.access_compmatdb() instead.
+        """
         if config is None:
             config = load_config()
         self._config = config
@@ -128,13 +165,23 @@ class Database(object):
 
     @property
     def formats_network(self):
+        "Returns the formats and adapter network."
         return self._formats_network
 
     @formats_network.setter
     def formats_network(self, value):
+        "Set the formats and adapter network."
         self._formats_network = value
 
     def _convert_src(self, src, method):
+        """Convert the :param src: object to a type expected by :param method:.
+
+        :param src: Arbitrary data type.
+        :param method: A python callable.
+
+        If :param method: is of type conversion.DBMethod, this function 
+        will attempt to convert :param src: to the :param method: expects type.
+        """
         if isinstance(method, conversion.DBMethod):
             try:
                 isinstance(src, method.expects)
@@ -165,6 +212,13 @@ class Database(object):
         return src
 
     def _update_cache(self, doc_ids, method):
+        """Attempt to update the cache for results of :param method: applied to data storen in documents with :param doc_ids:.
+
+        :param doc_ids: Document ids holding data.
+        :type doc_ids: A iterable of ObjectID.
+        :param method: The method to apply.
+        :type method: A python callable.
+        """
         docs = self._data.find({'_id': {'$in': list(doc_ids)}})
         records_skipped = docs.count()
         conversion_errors = 0
@@ -231,6 +285,7 @@ class Database(object):
             logger.debug(msg.format(m = method, n = records_skipped))
 
     def _split_filter(self, filter):
+        "Split filter into a standard MongoDB filter and a methods filter, which is locally resolved."
         if filter is None:
             return None, None
         else:
@@ -244,6 +299,12 @@ class Database(object):
             return standard_filter, methods_filter
 
     def _filter_by_method(self, doc_ids, method, expression):
+        """Apply :param method: to data in documents with :param doc_ids: and filter by :param expression:.
+
+        :param doc_ids: Documents to apply the filter to.
+        :param method: The method to apply.
+        :param expression: The query expression used for filtering.
+        """
         cache_spec = callable_spec(method)
         cache_spec[KEY_CACHE_DOC_ID] = {'$in': list(doc_ids)}
         if PYMONGO_3:
@@ -279,6 +340,10 @@ class Database(object):
             return set(doc['_id'] for doc in result['result'])
 
     def _filter_by_methods(self, docs, methods_filter):
+        """Apply the :param methods_filter: to all :param docs:.
+
+        See also: _filter_by_method(..)
+        """
         matching = set(doc['_id'] for doc in docs)
         for method, value in methods_filter.items():
             matching = self._filter_by_method(matching, method, value)
@@ -292,12 +357,14 @@ class Database(object):
         return matching
 
     def _add_metadata_from_context(self, metadata):
+        "Add implicit meta data. to the explicitely provided meta data."
         if not 'author_name' in metadata:
             metadata['author_name'] = self._config['author_name']
         if not 'author_email' in metadata:
             metadata['author_email'] = self._config['author_email']
 
     def _make_meta_document(self, metadata, data):
+        "Generate the records document from metadata and data."
         meta = copy.copy(metadata)
         if data is not None:
             meta[KEY_FILE_TYPE] = str(type(data))
@@ -305,9 +372,11 @@ class Database(object):
         return meta
 
     def _put_file(self, data):
+        "Store :param data: in a gridfs file."
         return self._gridfs.put(encode(data))
 
     def _insert_one(self, metadata, data):
+        "Insert a record associating metadata and data."
         meta = self._make_meta_document(metadata, data)
         if data is not None:
             file_id = self._put_file(data)
@@ -318,10 +387,12 @@ class Database(object):
             return self._data.insert(meta)
 
     def _get(self, file_id):
+        "Retrieve file with :param file_id: from the gridfs collection."
         grid_file = self._gridfs.get(file_id)
         return decode(grid_file.read())
 
     def _resolve_files(self, doc):
+        "Resolve the file data associated with doc."
         result = dict(doc)
         if KEY_FILE_ID in result:
             result[KEY_DOC_DATA] = self._get(result[KEY_FILE_ID])
@@ -332,9 +403,31 @@ class Database(object):
         return result
 
     def insert_one(self, document, data = None, * args, ** kwargs):
+        """Insert a new document into the database.
+
+        :param document: The metadata to be inserted into the database.
+        :param data: The data to associate with this record.
+
+        See also: 
+            - https://bitbucket.org/glotzer/compdb/wiki/latest/compmatdb_part2
+            - http://api.mongodb.org/python/current/api/pymongo/collection.html#pymongo.collection.Collection.insert_one
+        """
         self._insert_one(document, data, * args, ** kwargs)
 
     def replace_one(self, filter, replacement_data = None, upsert = False, * args, ** kwargs):
+        """Replace a document in the database.
+        
+        :param filter: The first document to match the filter will be replaced. The filter itself is the replacement metadata.
+        :type filter: A mapping type.
+        :param replacement_data: The replacement data.
+        :type replacement_data: Arbitrary binary data.
+        :param upsert: If true and no document matches the filter, a new document will be inserted into the database.
+        :type upsert: Boolean
+
+        See also: 
+            - https://bitbucket.org/glotzer/compdb/wiki/latest/compmatdb_part2
+            - http://api.mongodb.org/python/current/api/pymongo/collection.html#pymongo.collection.Collection.replace_one
+        """
         meta = self._make_meta_document(filter, replacement_data)
         to_be_replaced = self._data.find_one(meta)
         replacement = copy.copy(meta)
@@ -362,6 +455,17 @@ class Database(object):
             return result
 
     def update_one(self, document, data = None, * args, ** kwargs):
+        """Update document with new data.
+
+        :param document: The document to update.
+        :type document: A mappable type.
+        :param data: The data to replace the old data with.
+        :type data: Arbitrary binary data.
+        
+        See also: 
+            - https://bitbucket.org/glotzer/compdb/wiki/latest/compmatdb_part2
+            - http://api.mongodb.org/python/current/api/pymongo/collection.html#pymongo.collection.Collection.update_one
+        """
         meta = self._make_meta_document(document, data)
         if data is not None:
             file_id = self._put_file(data)
@@ -381,6 +485,18 @@ class Database(object):
                     self._gridfs.delete(to_be_updated[KEY_FILE_ID])
 
     def find(self, filter = None, projection = None, * args, ** kwargs):
+        """Find all records that match filter.
+
+        :param filter: The filter to match documents with.
+        :type filter: A mappable, that may contain callables as keys or values.
+        :param projection: A iterable of field names that the returned records contain.
+        :type projection: Iterable of str.
+        :raises UnsupportedExpressionError
+        
+        See also:
+            - https://bitbucket.org/glotzer/compdb/wiki/latest/compmatdb_part2
+            - http://api.mongodb.org/python/current/api/pymongo/collection.html#pymongo.collection.Collection.find
+        """
         call_dict = dict()
         plain_filter = self._resolve(filter, call_dict)
         docs = self._data.find(
@@ -388,6 +504,16 @@ class Database(object):
         return map(FileCursor(self, call_dict, projection), docs)
 
     def find_one(self, filter_or_id, projection = None, * args, ** kwargs):
+        """Like find(), but returns the first matching document or None if no document matches.
+        
+        :param filter_or_id: A filter or a document id.
+        :type filter_or_id: A mapping, which may contain callables as key or value or a str or ObjectID.
+        :param projection: A iterable of field names that the returned records contain.
+        :type projection: Iterable of str.
+        :raises UnsupportedExpressionError
+
+        See also: find()
+        """
         call_dict = dict()
         plain_filter_or_id = self._resolve(filter_or_id, call_dict)
         doc = self._data.find_one(plain_filter_or_id, projection, * args, ** kwargs)
@@ -397,19 +523,28 @@ class Database(object):
             return None
 
     def _resolve_doc(self, doc, call_dict, projection = None):
+        "Resolve a document containing callables."
         return self._resolve_projection(self._resolve_calls(self._resolve_files(doc), call_dict), projection)
 
     def _resolve_projection(self, doc, projection):
+        "Resolve a projection document for callables."
         if projection is None:
             return doc
         else:
             return {k: v for k,v in doc.items() if k in projection}
 
     def resolve(self, docs):
+        "Resolve the docs for file data."
+        warnings.warn("This function is obsolete.", DeprecationWarning)
+        raise Deprec
         for doc in docs:
             yield self._resolve_files(doc)
 
     def _delete_doc(self, doc):
+        """Delete document :param doc:.
+
+        This function removes both the metadata document as well as the filedata in the gridfs collection.
+        """
         if KEY_FILE_ID in doc:
             self._gridfs.delete(doc[KEY_FILE_ID])
         if PYMONGO_3:
@@ -418,16 +553,33 @@ class Database(object):
             self._cache.remove({KEY_FILE_ID: doc['_id']})
 
     def add_adapter(self, adapter):
+        """Add adapter :param adapter: to the formats and adapter network."""
         conversion.add_adapter_to_network(
             self._formats_network, adapter)
 
     def delete_one(self, filter, * args, ** kwargs):
+        """Delete the first document matching filter.
+
+        :param filter: The filter that the document to be deleted matches.
+        :type filter: A mapping type.
+        :raises UnsupportedExpressionError
+        
+        See also: delete_many()
+        """
         #doc = self._data.find_one_and_delete(filter, *args, **kwargs)
         doc = self._data.find_one(filter, *args, **kwargs)
         self._data.remove({'_id': doc['_id']})
         self._delete_doc(doc)
 
     def delete_many(self, filter, * args, ** kwargs):
+        """Delete all documents matching filter.
+
+        :param filter: The filter that all documents to be deleted match.
+        :type filter: A mapping type.
+        :raises UnsupportedExpressionError
+
+        See also: delete_one()
+        """
         docs = self._data.find(filter, *args, ** kwargs)
         for doc in docs:
             self._delete_doc(doc)
@@ -438,6 +590,7 @@ class Database(object):
         return result
 
     def _resolve_dict(self, d, call_dict, * args, ** kwargs):
+        "Resolve the dictionary for callables and other operators."
         standard = dict()
         methods_filter = dict()
         #methods_projection = dict()
@@ -463,6 +616,7 @@ class Database(object):
             return d
 
     def _resolve(self, expr, call_dict, *args, **kwargs):
+        "Resolve expression containing callables."
         if isinstance(expr, dict):
             plain = {k: self._resolve(v, call_dict, * args, ** kwargs)
                     for k,v in expr.items()}
@@ -477,13 +631,16 @@ class Database(object):
             return expr
 
     def _resolve_stage(self, stage, call_dict):
+        "Resolve a stage, which is part of a pipeline."
         return self._resolve(stage, call_dict)
 
     def _resolve_pipeline(self, pipeline, call_dict):
+        "Resolve a pipeline."
         for stage in pipeline:
             yield self._resolve_stage(stage, call_dict)
 
     def _resolve_calls(self, result, call_dict, data = None):
+        "Resolve all calls in result."
         if isinstance(result, dict):
             if KEY_FILE_ID in result:
                 data = self._get(result[KEY_FILE_ID])
@@ -514,6 +671,16 @@ class Database(object):
             return result
 
     def aggregate(self, pipeline, ** kwargs):
+        """Evaluate the aggregation pipeline.
+        
+        :param pipeline: The aggregation pipeline.
+        :type pipeline: An iterable of expressions.
+        :raises UnsupportedExpressionError
+
+        See also:
+            - https://bitbucket.org/glotzer/compdb/wiki/latest/compmatdb_part3
+            - http://api.mongodb.org/python/current/api/pymongo/collection.html#pymongo.collection.Collection.aggregate
+        """
         call_dict = dict()
         plain_pipeline = list(self._resolve_pipeline(pipeline, call_dict))
         logger.debug("Pipeline expression: '{}'.".format(plain_pipeline))
