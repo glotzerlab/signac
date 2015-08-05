4fba8c95394c7cb77e3e3047309ebf634cff21fb
Carl Simon Adorf <csadorf@umich.edu>
2015-08-05 11:36:55 -0400

    Bump up to version 0.1.5.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'compdb',
-    version = '0.1.5dev1',
+    version = '0.1.5',
     package_dir = {'': 'src'},
     packages = find_packages('src'),
 
