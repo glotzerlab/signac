f4c8f003a4ffdd8e64468d261aa2cd34d58f1b9d
Carl Simon Adorf <csadorf@umich.edu>
2015-08-28 21:57:32 -0400

    Add surrogate compdb package, linking to signac.
    Provided to guarantee compatibility.
    Prints warning on import.

--- src/compdb/__init__.py
+++ src/compdb/__init__.py
@@ -1,6 +1,8 @@
 import warnings
 
 from signac import *
+__all__ = ['core', 'contrib', 'db']
 
 msg = "compdb was renamed to signac. Please import signac in the future."
-warnings.warn(DeprecationWarning, msg)
+print('Warning!',msg)
+warnings.warn(msg, DeprecationWarning)
