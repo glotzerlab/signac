67492e3350b0442f12436fa6a9c4a97f3fb14180
Carl Simon Adorf <csadorf@umich.edu>
2015-09-04 16:35:35 -0400

    Add FileLink format.

--- src/signac/db/formats.py
+++ src/signac/db/formats.py
@@ -1,4 +1,8 @@
+import logging
+
 from . import conversion
+
+logger = logging.getLogger(__name__)
 
 BASICS = [int, float, str, bool]
 
@@ -23,6 +27,42 @@ class FileFormat(conversion.BasicFormat):
 
 class TextFile(FileFormat):
     pass
+
+class FileLink(conversion.BaseLink):
+    """Link format for file system files.
+
+    Derive from this class to specify link formats, that are accessible
+    through the local file system.
+
+    Attributes:
+        root: Specify the root path for this specific link type.
+              This allows to create file link formats for specific resources,
+              which might be linked into local file systems at different
+              locations.
+
+    .. example::
+
+        class HomeFileLink(FileLink):
+            from os.path import expanduser
+            root=expanduser('~')
+    """
+    root=''
+
+    def fetch(self):
+        "Load data from file at joined root and url path."
+        import os
+        fn = os.path.join(self.root, self.url)
+        try:
+            return open(fn, 'rb').read()
+        except FileNotFoundError as error:
+            msg = "Unable to open file '{}': {}. root='{}'"
+            logger.warning(msg.format(fn, error, self.root))
+            raise conversion.LinkError(error)
+
+    @classmethod
+    def set_root(cls, path):
+        "Change the root path of this file link type."
+        cls.root=path
 
 class FileToBytesAdapter(conversion.Adapter):
     expects = FileFormat
