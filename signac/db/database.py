34d32fe395bcce536d287be29194d0d8212876f4
Carl Simon Adorf <csadorf@umich.edu>
2015-10-22 15:06:36 -0400

    Late fail for usage of signac.db without pymongo.

--- signac/db/database.py
+++ signac/db/database.py
@@ -1,5 +1,8 @@
-from ..common import host
-
-
-def get_database(name, hostname=None, config=None):
-    return host.get_database(name=name, hostname=hostname, config=config)
+try:
+    from ..common import host
+except ImportError:
+    def get_database(name, hostname=None, config=None):
+        import pymongo  # noqa -- This will always fail.
+else:
+    def get_database(name, hostname=None, config=None):
+        return host.get_database(name=name, hostname=hostname, config=config)
