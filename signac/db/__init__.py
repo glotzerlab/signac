435ac230366e575057d46f15add4765d433a55e1
Carl Simon Adorf <csadorf@umich.edu>
2015-10-22 21:20:52 -0400

    Explicit check for pymongo in db package __init__ module.

--- signac/db/__init__.py
+++ signac/db/__init__.py
@@ -1,9 +1,10 @@
 import warnings
 try:
-    from .database import get_database
+    import pymongo
 except ImportError:
     warnings.warn("Failed to import pymongo. "
                   "get_database will not be available.", ImportWarning)
     __all__ = []
 else:
+    from .database import get_database
     __all__ = ['get_database']
