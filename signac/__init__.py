c8e7400008f19f89519cbfd067c8e82f41fc503a
Carl Simon Adorf <csadorf@umich.edu>
2015-10-23 09:58:42 -0400

    Put contrib and db into global API.

--- signac/__init__.py
+++ signac/__init__.py
@@ -6,6 +6,9 @@ The framework provides a simple data model, which helps to organize
 data production and post-processing as well as distribution among collaboratos.
 """
 
+from . import contrib
+from . import db
+
 # The VERSION string represents the actual (development) version of the
 # package.
 VERSION = '0.1.7'
@@ -13,4 +16,4 @@ VERSION = '0.1.7'
 # required to be updated and can therefore lag behind the actual version.
 VERSION_TUPLE = 0, 1, 7
 
-__all__ = ['VERSION', 'VERSION_TUPLE']
+__all__ = ['VERSION', 'VERSION_TUPLE', 'contrib', 'db']
