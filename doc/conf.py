4227e76da31f01ad8cf25ef12c739e2a7b674686
Carl Simon Adorf <csadorf@umich.edu>
2015-10-26 14:02:42 -0400

    Configure sphinx to use mock modules.

--- doc/conf.py
+++ doc/conf.py
@@ -17,6 +17,15 @@ import os
 import shlex
 
 import sphinx_rtd_theme
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return Mock()
+
+MOCK_MODULES = ['pymongo', 'mpi4py', 'networkx']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
