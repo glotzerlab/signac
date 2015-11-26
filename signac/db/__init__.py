88922bb769ae7e684870a41bab6faec4947cc3e7
Carl Simon Adorf <csadorf@umich.edu>
2015-11-26 13:00:35 -0500

    More meaningful error message when pymongo is missing.
    Fixes issue 11.

--- signac/db/__init__.py
+++ signac/db/__init__.py
@@ -4,7 +4,15 @@ try:
 except ImportError:
     warnings.warn("Failed to import pymongo. "
                   "get_database will not be available.", ImportWarning)
-    __all__ = []
+
+    def get_database(*args, **kwargs):
+        """Get a database handle.
+
+        This function is only available if pymongo is installed."""
+        raise ImportError(
+            "You need to install pymongo to use `get_database()`.")
 else:
     from .database import get_database
-    __all__ = ['get_database']
+
+
+__all__ = ['get_database']
