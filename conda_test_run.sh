a6141bfc34dfcc0aaad7501ac39c19afe36a21b3
Carl Simon Adorf <csadorf@umich.edu>
2015-11-28 22:55:10 -0500

    Pass arguments to conda test runner.

--- conda_test_run.sh
+++ conda_test_run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 source activate signac-py27
-python -m unittest discover tests
+python -m unittest discover tests ${@}
 source activate signac-py33
-python -m unittest discover tests
+python -m unittest discover tests ${@}
 source activate signac-py34
-python -m unittest discover tests
+python -m unittest discover tests ${@}
 source activate signac-py35
-python -m unittest discover tests
+python -m unittest discover tests ${@}
