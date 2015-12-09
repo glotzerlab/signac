d2bffd6288b49a2d2fc5b155a76810748ea0b991
Carl Simon Adorf <csadorf@umich.edu>
2015-12-09 09:46:19 -0500

    Bump up version to 0.2.3.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='signac',
-    version='0.2.3dev1',
+    version='0.2.3',
     packages=find_packages(),
 
     author='Carl Simon Adorf',
--- signac/__init__.py
+++ signac/__init__.py
@@ -10,6 +10,6 @@ from . import contrib
 from . import db
 from . import gui
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 __all__ = ['__version__', 'contrib', 'db', 'gui']
