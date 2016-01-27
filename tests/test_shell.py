00ed2740443a6a64f31194847915c0938ad12cd3
Carl Simon Adorf <csadorf@umich.edu>
2016-01-27 16:20:50 -0500

    Provide tests for shell API.

---
+++ tests/test_shell.py
@@ -0,0 +1,102 @@
+import os
+import sys
+import io
+import unittest
+import subprocess
+import warnings
+import six
+
+import signac
+
+if six.PY3:
+    from tempfile import TemporaryDirectory
+else:
+    from tempdir import TemporaryDirectory
+
+
+class DummyFile(object):
+    "We redirect sys stdout into this file during console tests."
+    def __init__(self):
+        self._x = ''
+
+    def write(self, x):
+        self._x += x
+
+    def flush(self): pass
+
+    def read(self): 
+        x = self._x
+        self._x = ''
+        return x
+
+class ExitCodeError(RuntimeError): pass
+
+class BasicShellTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.tmpdir = TemporaryDirectory(prefix='signac_')
+        self.addCleanup(self.tmpdir.cleanup)
+
+    def call(self, command, input=None):
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate(input=input)
+        if p.returncode != 0:
+            raise ExitCodeError()
+        if six.PY2:
+            return str(out)
+        else:
+            return out.decode()
+
+    def test_init_project(self):
+        os.chdir(self.tmpdir.name)
+        self.call('signac init my_project'.split())
+        assert str(signac.get_project()) == 'my_project'
+
+    def test_init_project_in_project_root(self):
+        os.chdir(self.tmpdir.name)
+        self.call('signac init my_project'.split())
+        assert str(signac.get_project()) == 'my_project'
+        with self.assertRaises(ExitCodeError):
+            self.call('signac init second_project'.split())
+
+    def test_project_id(self):
+        os.chdir(self.tmpdir.name)
+        self.call('signac init my_project'.split())
+        self.assertEqual(str(signac.get_project()), 'my_project')
+        self.assertEqual(self.call('signac project'.split()).strip(), 'my_project')
+
+    def test_project_workspace(self):
+        os.chdir(self.tmpdir.name)
+        self.call('signac init my_project'.split())
+        self.assertEqual(str(signac.get_project()), 'my_project')
+        self.assertEqual(
+            self.call('signac project --workspace'.split()).strip(),
+            os.path.join(self.tmpdir.name, 'workspace'))
+
+    def test_job_with_argument(self):
+        os.chdir(self.tmpdir.name)
+        self.call('signac init my_project'.split())
+        self.assertEqual(
+            self.call(['signac', 'job', '{"a": 0}']).strip(),
+            '9bfd29df07674bc4aa960cf661b5acd2')
+
+    def test_job_with_argument_workspace(self):
+        os.chdir(self.tmpdir.name)
+        self.call('signac init my_project'.split())
+        wd_path = os.path.join(self.tmpdir.name, 'workspace', '9bfd29df07674bc4aa960cf661b5acd2')
+        self.assertEqual(
+            self.call(['signac', 'job', '--workspace', '{"a": 0}']).strip(), wd_path)
+
+    def test_job_with_argument_create_workspace(self):
+        os.chdir(self.tmpdir.name)
+        self.call('signac init my_project'.split())
+        wd_path = os.path.join(self.tmpdir.name, 'workspace', '9bfd29df07674bc4aa960cf661b5acd2')
+        self.assertFalse(os.path.isdir(wd_path))
+        self.call(['signac', 'job', '--create', '{"a": 0}'])
+        self.assertTrue(os.path.isdir(wd_path))
+
+        
+        
+
+if __name__ == '__main__':
+    unittest.main()
