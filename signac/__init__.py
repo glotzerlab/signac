f3a8bad2e8a81326e00d92d935bd23f43495648d
Carl Simon Adorf <csadorf@umich.edu>
2016-08-31 15:40:19 -0400

    Bump up version number to 0.5.0.

--- signac/__init__.py
+++ signac/__init__.py
@@ -17,7 +17,7 @@ from . import gui
 from .contrib import Project, get_project, init_project, fetch, fetch_one
 from .db import get_database
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 __all__ = ['__version__', 'contrib', 'db', 'gui',
            'Project', 'get_project', 'init_project',
