e4397a07c56c7da2740f47a0228b2326922c0e09
Carl Simon Adorf <csadorf@umich.edu>
2016-11-17 07:54:11 -0800

    Put indexing module in contrib namespace.
    Fix logging bug introduced in earlier commit.

--- signac/contrib/__init__.py
+++ signac/contrib/__init__.py
@@ -6,14 +6,26 @@ import logging
 
 from . import conversion
 from . import formats
+from . import indexing
 from .project import Project, get_project, init_project
-from .crawler import BaseCrawler, RegexFileCrawler, JSONCrawler,\
-    SignacProjectCrawler, MasterCrawler, fetch, fetch_one, fetched,\
-    export_one, export, export_to_mirror, export_pymongo
+from .indexing import BaseCrawler
+from .indexing import RegexFileCrawler
+from .indexing import JSONCrawler
+from .indexing import SignacProjectCrawler
+from .indexing import MasterCrawler
+from .indexing import fetch
+from .indexing import fetch_one  # deprecated
+from .indexing import fetched
+from .indexing import export_one
+from .indexing import export
+from .indexing import export_to_mirror
+from .indexing import export_pymongo
+
+logger = logging.getLogger(__name__)
 
 
 __all__ = [
-    'conversion', 'formats',
+    'conversion', 'formats', 'indexing',
     'Project', 'get_project', 'init_project',
     'BaseCrawler', 'RegexFileCrawler', 'JSONCrawler', 'SignacProjectCrawler',
     'MasterCrawler', 'fetch', 'fetch_one', 'fetched',
@@ -23,7 +35,7 @@ __all__ = [
 try:
     import networkx  # noqa
 except ImportError:
-    logging.debug("Failed to import networkx. formats_network will "
+    logger.debug("Failed to import networkx. formats_network will "
                   "not be available.")
 else:
     from .formats_network import get_formats_network, get_conversion_network  # noqa
@@ -32,7 +44,7 @@ else:
 try:
     import mpi4py  # noqa
 except ImportError:
-    logging.debug("Failed to import mpi4py. MPIPool will not be available.")
+    logger.debug("Failed to import mpi4py. MPIPool will not be available.")
 else:
     from .mpipool import MPIPool  # noqa
     __all__.extend(['MPIPool'])
