a780273b2d598cf237759fe49261d0e98710a4a1
Carl Simon Adorf <csadorf@umich.edu>
2016-12-28 23:40:29 -0500

    Expose DestinationExistsError and MergeConflict in root error namespace.

--- signac/errors.py
+++ signac/errors.py
@@ -3,11 +3,15 @@
 # This software is licensed under the BSD 3-Clause License.
 
 from .core.errors import Error
+
 from .common.errors import ConfigError
 from .common.errors import AuthenticationError
 from .common.errors import ExportError
 from .common.errors import FileNotFoundError
 from .common.errors import FetchError
+
+from .contrib.errors import DestinationExistsError
+from .contrib.errors import MergeConflict
 
 __all__ = [
     'Error',
@@ -16,4 +20,6 @@ __all__ = [
     'ExportError',
     'FileNotFoundError',
     'FetchError',
+    'DestinationExistsError',
+    'MergeConflict',
 ]
