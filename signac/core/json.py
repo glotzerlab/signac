a901d6847decf805ab2f83f5d9f7e7f0cdeaf71b
Carl Simon Adorf <csadorf@umich.edu>
2017-01-07 20:09:32 -0500

    Add python-rapidjson as alternative JSON encoder/decoder.
    Whenever the alternative package is installed it will be used as a
    drop-in replacement for the standard json module.

--- signac/core/json.py
+++ signac/core/json.py
@@ -1,7 +1,17 @@
 # Copyright (c) 2017 The Regents of the University of Michigan
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
-import json
+import logging
+
+logger = logging.getLogger(__name__)
+msg = "Using '{}' package for JSON encoding/decoding."
+
+try:
+    import rapidjson as json
+    logger.debug(msg.format('rapidjson'))
+except ImportError:
+    import json
+    logger.debug(msg.format('json'))
 
 
 __all__ = ['json']
