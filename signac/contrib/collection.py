a8011c8cf78c951c28290db16e41e2e9d02fba6e
Carl Simon Adorf <csadorf@umich.edu>
2017-03-20 07:45:55 -0400

    Implement search/indexing capabilities directly into Collection.

--- signac/contrib/collection.py
+++ signac/contrib/collection.py
@@ -1,16 +1,93 @@
+# Copyright (c) 2017 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
 import sys
 import io
 import logging
+from collections import defaultdict
 
-from signac.core.search_engine import DocumentSearchEngine
-from signac.core.json import json
+from ..core.json import json
+from ..common import six
+if six.PY2:
+    from collections import Mapping
+else:
+    from collections.abc import Mapping
 
 
 logger = logging.getLogger(__name__)
 
 
-def _index(docs):
-    return {doc['_id']: doc for doc in docs}
+def _index(docs, key):
+    return {doc[key]: doc for doc in docs}
+
+
+def _flatten(container):
+    for i in container:
+        if isinstance(i, (list, tuple)):
+            for j in _flatten(i):
+                yield j
+        else:
+            yield i
+
+
+def _encode_tree(x):
+    if isinstance(x, list):
+        return json.dumps(x)
+    else:
+        return x
+
+
+def _traverse_tree(t, include=None, encode=None):
+    if encode is not None:
+        t = encode(t)
+    if include is False:
+        return
+    if isinstance(t, list):
+        for i in t:
+            for b in _traverse_tree(i, include, encode):
+                yield b
+    elif isinstance(t, Mapping):
+        for k in t:
+            if include is None or include is True:
+                for i in _traverse_tree(t[k], encode=encode):
+                    yield k, i
+            else:
+                if not include.get(k, False):
+                    continue
+                for i in _traverse_tree(t[k], include.get(k), encode=encode):
+                    yield k, i
+    else:
+        yield t
+
+
+def _traverse_filter(t, include=None):
+    for b in _traverse_tree(t, include=include, encode=_encode_tree):
+        yield b
+
+
+def _valid_filter(f, top=True):
+    if isinstance(f, Mapping):
+        return all(_valid_filter(v, top=False) for v in f.values())
+    elif isinstance(f, list):
+        return not top
+    else:
+        return True
+
+
+def _build_index(docs, key, primary_key):
+    nodes = key.split('.')
+    index = defaultdict(set)
+
+    def _get_value(doc, nodes):
+        if nodes:
+            return _get_value(doc[nodes[0]], nodes[1:])
+        else:
+            return doc
+
+    for doc in docs:
+        v = _get_value(doc, nodes)
+        index[_encode_tree(v)].add(doc[primary_key])
+    return index
 
 
 class _CollectionSearchResults(object):
@@ -29,50 +106,140 @@ class _CollectionSearchResults(object):
 
 
 class Collection(object):
+    primary_key = '_id'
 
     def __init__(self, docs=None):
         self._file = io.StringIO()
-        self._dirty = False
-        self._engine = None
+        self._dirty = set()
+        self._indeces = dict()
         if docs is None:
             self._docs = dict()
         else:
-            self._docs = _index(docs)
+            self._docs = _index(docs, self.primary_key)
 
     def _assert_open(self):
         if self._docs is None:
             raise RuntimeError("Trying to access closed {}.".format(
                 type(self).__name__))
 
+    def _remove_from_indeces(self, _id):
+        for index in self._indeces.values():
+            remove_keys = set()
+            for key, group in index.items():
+                try:
+                    group.remove(_id)
+                except KeyError:
+                    pass
+                if not len(group):
+                    remove_keys.add(key)
+            for key in remove_keys:
+                del index[key]
+
+    def _update_indeces(self):
+        if self._dirty:
+            for _id in self._dirty:
+                self._remove_from_indeces(_id)
+            docs = [self[_id] for _id in self._dirty]
+            for key in self._indeces:
+                self._indeces[key].update(_build_index(docs, key, self.primary_key))
+            self._dirty.clear()
+
+    def _build_index(self, key):
+        logger.debug("Building index for key '{}'...".format(key))
+        self._indeces[key] = _build_index(self._docs.values(), key, self.primary_key)
+        logger.debug("Built index for key '{}'.".format(key))
+
+    def index(self, key, build=False):
+        if key == self.primary_key:
+            raise KeyError("Can't access index for primary key via index() method.")
+        elif key not in self._indeces:
+            if build:
+                self._build_index(key)
+            else:
+                raise KeyError("No index for key '{}'.".format(key))
+        self._update_indeces()
+        return self._indeces[key]
+
     def __str__(self):
         return "<{} file={}>".format(type(self).__name__, self._file)
 
     def __iter__(self):
         self._assert_open()
-        return iter(self._docs.values())
+        return iter(self._docs)
 
     def __len__(self):
         self._assert_open()
         return len(self._docs)
 
+    def __contains__(self, _id):
+        return _id in self._docs
+
     def __getitem__(self, _id):
         self._assert_open()
-        return self._docs[_id]
+        return self._docs[_id].copy()
 
     def __setitem__(self, _id, doc):
         self._assert_open()
+        if not isinstance(_id, int):
+            raise TypeError("The primary key must be an integer type!")
+        doc.setdefault(self.primary_key, _id)
+        if doc[self.primary_key] != _id:
+            raise ValueError("Primary key ('{}') mismatch!".format(self.primary_key))
+        doc = json.loads(json.dumps(doc))
+        self._dirty.add(_id)
         self._docs[_id] = doc
-        self._dirty = True
 
-    def _find(self, filter):
+    def __delitem__(self, _id):
         self._assert_open()
-        if len(filter) == 1 and '_id' in filter:
-            return (filter['_id'], )
-        if self._engine is None:
-            self._engine = DocumentSearchEngine(self._docs.values())
-        return self._engine.find(filter)
+        del self._docs[_id]
+        self._remove_from_indeces(_id)
+        try:
+            self._dirty.remove(_id)
+        except KeyError:
+            pass
 
-    def find(self, filter):
+    def clear(self):
+        self._docs.clear()
+        self._indeces.clear()
+        self._dirty.clear()
+
+    def update(self, docs):
+        for doc in docs:
+            doc.setdefault(self.primary_key, len(self))
+            self[doc[self.primary_key]] = doc
+
+    def _check_filter(self, filter):
+        if filter is None:
+            return True
+        if not _valid_filter(filter):
+            raise ValueError(filter)
+
+    def _find(self, filter=None):
+        filter = json.loads(json.dumps(filter))  # Normalize
+        self._check_filter(filter)
+        if filter is None or not len(filter):
+            return self._docs.keys()
+        _id = filter.pop(self.primary_key, None)
+        if _id is not None and _id in self:
+            result = {_id}
+        else:
+            result = None
+        for branch in _traverse_filter(filter):
+            nodes = list(_flatten(branch))
+            key = '.'.join(nodes[:-1])
+            value = nodes[-1]
+            index = self.index(key, build=True)
+            matches = index.get(value, set())
+            if result is None:
+                result = matches
+            else:
+                result = result.intersection(matches)
+            if not result:
+                break
+        return result
+
+    def find(self, filter=None):
+        """Find all documents matching filter."""
         return _CollectionSearchResults(self, self._find(filter))
 
     def replace_one(self, filter, doc, upsert=False):
@@ -97,12 +264,13 @@ class Collection(object):
         return collection
 
     @classmethod
-    def open(cls, filename, mode='r+'):
+    def open(cls, filename, mode='a+'):
         logger.debug("Open collection '{}'.".format(filename))
         if filename == ':memory:':
             file = io.StringIO()
         else:
             file = open(filename, mode)
+            file.seek(0)
         return cls._open(file)
 
     def flush(self):
@@ -123,6 +291,7 @@ class Collection(object):
         if self._file is not None:
             self.flush()
             self._file.close()
+            self._indeces.clear()
             self._docs = None
             self._file = None
 
@@ -130,7 +299,4 @@ class Collection(object):
         return self
 
     def __exit__(self, t, v, tb):
-        self.close()
-
-    def __del__(self):
         self.close()
---
+++ tests/test_collection.py
@@ -0,0 +1,178 @@
+import os
+import unittest
+
+from signac import Collection
+from signac.common import six
+if six.PY2:
+    from tempdir import TemporaryDirectory
+else:
+    from tempfile import TemporaryDirectory
+
+
+class CollectionTest(unittest.TestCase):
+
+    def setUp(self):
+        self.c = Collection()
+
+    def test_init(self):
+        self.assertEqual(len(self.c), 0)
+
+    def test_insert(self):
+        doc = dict(a=0)
+        self.c[0] = doc
+        self.assertEqual(len(self.c), 1)
+        self.assertEqual(self.c[0], doc)
+        self.assertEqual(list(self.c.find()), [doc])
+        with self.assertRaises(ValueError):
+            self.c[1] = dict(_id=0)
+        with self.assertRaises(TypeError):
+            self.c['1'] = dict(a=0)
+        with self.assertRaises(TypeError):
+            self.c[1.0] = dict(a=0)
+
+    def test_insert_and_remove(self):
+        doc = dict(a=0)
+        self.c[0] = doc
+        self.assertEqual(len(self.c), 1)
+        self.assertEqual(self.c[0], doc)
+        self.assertEqual(list(self.c.find()), [doc])
+        del self.c[0]
+        self.assertEqual(len(self.c), 0)
+        with self.assertRaises(KeyError):
+            self.assertEqual(self.c[0], doc)
+
+    def test_contains(self):
+        self.assertFalse(0 in self.c)
+        self.c[0] = dict()
+        self.assertTrue(0 in self.c)
+        del self.c[0]
+        self.assertFalse(0 in self.c)
+        docs = [dict(_id=i) for i in range(10)]
+        self.c.update(docs)
+        for _id in self.c:
+            self.assertTrue(_id in self.c)
+        for doc in docs:
+            self.assertTrue(doc['_id'] in self.c)
+
+    def test_update(self):
+        docs = [dict(a=i) for i in range(10)]
+        self.c.update(docs)
+        self.assertEqual(len(self.c), len(docs))
+
+    def test_index(self):
+        docs = [dict(a=i) for i in range(10)]
+        self.c.update(docs)
+        with self.assertRaises(KeyError):
+            index = self.c.index('a')
+        index = self.c.index('a', build=True)
+        self.assertEqual(len(index), len(self.c))
+        for value, _ids in index.items():
+            for _id in _ids:
+                self.assertEqual(self.c[_id]['a'], value)
+        del self.c[0]
+        self.assertEqual(len(self.c), len(docs)-1)
+        index = self.c.index('a', build=True)
+        self.assertEqual(len(index), len(self.c))
+        for value, _ids in index.items():
+            for _id in _ids:
+                self.assertEqual(self.c[_id]['a'], value)
+        self.c[0] = docs[0]
+        index = self.c.index('a', build=True)
+        self.assertEqual(len(index), len(self.c))
+        for value, _ids in index.items():
+            for _id in _ids:
+                self.assertEqual(self.c[_id]['a'], value)
+        self.c[0] = dict(a=-1)
+        index = self.c.index('a', build=True)
+        self.assertEqual(len(index), len(self.c))
+        for value, _ids in index.items():
+            for _id in _ids:
+                self.assertEqual(self.c[_id]['a'], value)
+
+    def test_clear(self):
+        self.assertEqual(len(self.c), 0)
+        self.c[0] = dict(a=0)
+        self.assertEqual(len(self.c), 1)
+        self.c.clear()
+        self.assertEqual(len(self.c), 0)
+
+    def test_iteration(self):
+        self.assertEqual(len(self.c), 0)
+        self.assertEqual(len(list(self.c.find())), 0)
+        docs = self.c[0] = dict(a=0)
+        self.assertEqual(len(self.c), 1)
+        self.assertEqual(len(list(self.c.find())), 1)
+        self.c.clear()
+        docs = [dict(a=i) for i in range(10)]
+        self.c.update(docs)
+        self.assertEqual(len(self.c), len(docs))
+        self.assertEqual(len(list(self.c.find())), len(docs))
+        self.assertEqual(list(self.c.find()), docs)
+
+    def test_find(self):
+        self.assertEqual(len(list(self.c.find())), 0)
+        self.assertEqual(list(self.c.find()), [])
+        self.assertEqual(len(list(self.c.find({'a': 0}))), 0)
+        self.assertEqual(list(self.c.find()), [])
+        docs = [dict( a=i) for i in range(10)]
+        self.c.update(docs)
+        self.assertEqual(len(list(self.c.find())), len(docs))
+        self.assertEqual(list(self.c.find()), docs)
+        self.assertEqual(len(list(self.c.find({'a': 0}))), 1)
+        self.assertEqual(list(self.c.find({'a': 0}))[0], docs[0])
+        self.assertEqual(len(list(self.c.find({'a': -1}))), 0)
+        del self.c[0]
+        self.assertEqual(len(list(self.c.find({'a': 0}))), 0)
+
+    def test_find_types(self):
+        # Note: All of the iterables will be normalized to lists!
+        t = [1, 1.0, '1', [1], tuple([1])]
+        for i, t in enumerate(t):
+            self.c.clear()
+            doc = self.c[i] = dict( a=t)
+            self.assertEqual(list(self.c.find(doc)), [self.c[i]])
+
+    def test_find_nested(self):
+        docs = [dict(a=dict(b=i)) for i in range(10)]
+        self.c.update(docs)
+        self.assertEqual(len(list(self.c.find())), len(docs))
+        self.assertEqual(list(self.c.find()), docs)
+        self.assertEqual(len(list(self.c.find({'a.b': 0}))), 1)
+        self.assertEqual(len(list(self.c.find({'a': {'b': 0}}))), 1)
+        self.assertEqual(list(self.c.find({'a.b': 0}))[0], docs[0])
+        del self.c[0]
+        self.assertEqual(len(list(self.c.find({'a.b': 0}))), 0)
+        self.assertEqual(len(list(self.c.find({'a': {'b': 0}}))), 0)
+
+    def test_replace_one(self):
+        docs = [dict(a=i) for i in range(10)]
+        docs_ = [dict(a=-i) for i in range(10)]
+        self.c.update(docs)
+        for doc, doc_ in zip(self.c.find(), docs_):
+            self.c.replace_one(doc, doc_)
+        self.assertEqual(len(self.c), len(docs))
+        self.assertEqual(len(list(self.c.find())), len(docs))
+        self.assertEqual(list(self.c.find()), docs_)
+
+
+class FileCollectionTest(CollectionTest):
+
+    def setUp(self):
+        self._tmp_dir = TemporaryDirectory(prefix='signac_collection_')
+        self._fn_collection = os.path.join(self._tmp_dir.name, 'test.txt')
+        self.addCleanup(self._tmp_dir.cleanup)
+        self.c = Collection.open(self._fn_collection)
+        self.addCleanup(self.c.close)
+
+    def test_reopen(self):
+        docs = [dict(_id=i) for i in range(10)]
+        self.c.update(docs)
+        self.c.flush()
+        with Collection.open(self._fn_collection) as c:
+            self.assertEqual(len(c), len(self.c))
+            for _id in self.c:
+                self.assertTrue(_id in c)
+
+
+if __name__ == '__main__':
+    unittest.main()
