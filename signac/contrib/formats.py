b2990e9f746fb4121d8af82d41b681e6d8f100de
Carl Simon Adorf <csadorf@umich.edu>
2017-08-19 22:37:50 -0400

    Remove the formats module.

--- signac/contrib/formats.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2017 The Regents of the University of Michigan
-# All rights reserved.
-# This software is licensed under the BSD 3-Clause License.
-
-TextFile = 'TextFile'
-"A standard text file format."
