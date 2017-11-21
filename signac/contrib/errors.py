57f968b52f46b75ee3541a7c2df1d20f54289394
Jens Glaser <jsglaser@umich.edu>
2017-11-21 10:15:03 -0500

    add a missing underscore

--- signac/contrib/errors.py
+++ signac/contrib/errors.py
@@ -20,6 +20,6 @@ class DestinationExistsError(Error, RuntimeError):
 
 class JobsCorruptedError(Error, RuntimeError):
     "The state point manifest file of one or more jobs cannot be openend or is corrupted."
-    def __init_(self, job_ids):
+    def __init__(self, job_ids):
         self.job_ids = job_ids
         "The job id(s) of the corrupted job(s)."
