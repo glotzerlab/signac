0bb86b5d4e858d9cc6aa45c598988c8f0a86a27b
Carl Simon Adorf <csadorf@umich.edu>
2017-12-16 10:33:25 -0500

    Fix py27 issue in contrib/utility module.

--- signac/contrib/utility.py
+++ signac/contrib/utility.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2017 The Regents of the University of Michigan
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
+from __future__ import print_function
 import logging
 import sys
 import os
