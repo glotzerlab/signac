fc0c1bc88a04e5df4ba825b4b19840366fb021c3
Carl Simon Adorf <csadorf@umich.edu>
2018-02-26 22:23:39 -0500

    Limit the global JSONDict buffer size.
    This patch enables users to overwrite the default buffer size, objects
    which are too large are not stored in the buffer and the buffer is
    automatically flushed if adding new values would lead to exceeding the
    buffer size.

--- signac/core/jsondict.py
+++ signac/core/jsondict.py
@@ -3,8 +3,10 @@
 # This software is licensed under the BSD 3-Clause License.
 "Dict implementation with backend JSON file."
 import os
+import sys
 import errno
 import uuid
+import logging
 from contextlib import contextmanager
 
 from .json import json
@@ -12,24 +14,36 @@ from .attrdict import SyncedAttrDict
 from ..common import six
 
 
+logger = logging.getLogger(__name__)
+
+DEFAULT_BUFFER_SIZE = 32 * 2**20    # 32 MB
+
 _BUFFER_MODE = 0
+_BUFFER_SIZE = None
 _JSONDICT_BUFFER = dict()
 
 
 def flush_all():
     "Execute all deferred JSONDict write operations."
+    logger.debug("Flushing buffer...")
     while _JSONDICT_BUFFER:
         filename, blob = _JSONDICT_BUFFER.popitem()
         with open(filename, 'wb') as file:
             file.write(blob)
 
 
+def get_buffer_size():
+    "Returns the current read/write buffer size."
+    return sum((sys.getsizeof(x) for x in _JSONDICT_BUFFER.values()))
+
+
 def in_buffer_mode():
+    "Return true if in buffered read/write mode."
     return _BUFFER_MODE > 0
 
 
 @contextmanager
-def buffer_reads_writes():
+def buffer_reads_writes(buffer_size=DEFAULT_BUFFER_SIZE):
     """Enter a global buffer mode for all JSONDict instances.
 
     All future read operations will be suspended after the first
@@ -38,7 +52,13 @@ def buffer_reads_writes():
     All write operations are deferred until after leaving the buffer mode.
     """
     global _BUFFER_MODE
+    global _BUFFER_SIZE
     assert _BUFFER_MODE >= 0
+
+    if _BUFFER_SIZE is not None and _BUFFER_SIZE != buffer_size:
+        logger.warn("Buffer size already set, ignoring new value.")
+    else:
+        _BUFFER_SIZE = buffer_size
 
     _BUFFER_MODE += 1
     try:
@@ -46,6 +66,7 @@ def buffer_reads_writes():
     finally:
         _BUFFER_MODE -= 1
         if _BUFFER_MODE == 0:
+            _BUFFER_SIZE = None
             flush_all()
 
 
@@ -66,10 +87,10 @@ class JSONDict(SyncedAttrDict):
     def _load(self):
         assert self._filename is not None
 
-        if _BUFFER_MODE > 0:            # buffered mode
-            if self._filename in _JSONDICT_BUFFER:
-                return json.loads(_JSONDICT_BUFFER[self._filename].decode())
-        else:                           # non-buffered mode
+        if _BUFFER_MODE > 0 and self._filename in _JSONDICT_BUFFER:
+            # Load from buffer:
+            return json.loads(_JSONDICT_BUFFER[self._filename].decode())
+        else:   # Load from disk:
             try:
                 with open(self._filename, 'rb') as file:
                     return json.loads(file.read().decode())
@@ -83,9 +104,13 @@ class JSONDict(SyncedAttrDict):
         # Serialize data:
         blob = json.dumps(self._as_dict()).encode()
 
-        if _BUFFER_MODE > 0:            # buffered mode
+        if _BUFFER_MODE > 0 and sys.getsizeof(blob) <= _BUFFER_SIZE:
+            # Saving in buffer:
+            if sys.getsizeof(blob) + get_buffer_size() > _BUFFER_SIZE:
+                logger.debug("Buffer size exceeds limit, flushing...")
+                flush_all()
             _JSONDICT_BUFFER[self._filename] = blob
-        else:                           # non-buffered mode
+        else:   # Saving to disk:
             if self._write_concern:
                 dirname, filename = os.path.split(self._filename)
                 fn_tmp = os.path.join(dirname, '._{uid}_{fn}'.format(
