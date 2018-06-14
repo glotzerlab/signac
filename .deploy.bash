c2c70957d600ac4cbb58532d49fa5f32a078fc54
Carl Simon Adorf <csadorf@umich.edu>
2018-06-14 14:18:51 -0400

    Update the deployment scripts.

--- .deploy.bash
+++ .deploy.bash
@@ -8,7 +8,7 @@ python -m pip install -U twine wheel setuptools
 # PYPI_USERNAME - (Required) Username for the publisher's account on PyPI
 # PYPI_PASSWORD - (Required, Secret) Password for the publisher's account on PyPI
 
-cat <<'EOF' >> ~/.pypirc
+cat << EOF >> ~/.pypirc
 [distutils]
 index-servers=
     pypi
@@ -16,13 +16,13 @@ index-servers=
 
 [pypi]
 repository: https://pypi.python.org/pypi
-username: $PYPI_USERNAME
-password: $PYPI_PASSWORD
+username: ${PYPI_USERNAME}
+password: ${PYPI_PASSWORD}
 
 [testpypi]
 repository: https://test.pypi.org/legacy/
-username: $PYPI_USERNAME
-password: $PYPI_PASSWORD
+username: ${PYPI_USERNAME}
+password: ${PYPI_PASSWORD}
 EOF
 
 python setup.py bdist_wheel
