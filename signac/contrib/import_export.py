842d6ce5a2149e120d0c1e3078005e135228f585
Carl Simon Adorf <csadorf@umich.edu>
2018-10-23 09:37:34 -0400

    Provide better error handling and message for issue #93.
    This commit is not really resolving the issue, but instead provides the
    users some hints on how to resolve it.

--- signac/contrib/import_export.py
+++ signac/contrib/import_export.py
@@ -118,7 +118,15 @@ def _make_path_function(jobs, path):
             try:
                 try:
                     ret = path.format(job=job, **job.sp)
-                except TypeError:
+                except TypeError as error:
+                    if str(error) == "format() got multiple values for keyword argument 'job'":
+                        try:
+                            ret = path.format(job=job)
+                        except KeyError:
+                            raise _SchemaPathEvaluationError(
+                                "You must use fully qualified fields for this path, because the "
+                                "state point contains a key called 'job', e.g.: '{job.sp.job}' "
+                                "instead of '{job}'.")
                     ret = path.format(job=job)
                 return _AutoPathFormatter(paths).format(ret, auto=job)
             except AttributeError as error:
