263b1a55d463ffb230951b63c92fa2ee358fb6ac
Carl Simon Adorf <csadorf@umich.edu>
2019-02-08 17:45:17 -0500

    Use internal _mode property in open().

--- signac/core/h5store.py
+++ signac/core/h5store.py
@@ -310,7 +310,7 @@ class H5Store(MutableMapping):
         import h5py
         self._thread_lock.acquire()
         try:
-            self._file = h5py.File(self._filename, mode=self.mode, **self._kwargs)
+            self._file = h5py.File(self._filename, mode=self._mode, **self._kwargs)
         except:  # noqa We need to release under **all** circumstances upon error!
             self._thread_lock.release()
             raise
