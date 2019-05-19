2eea37cc8a060b222b66aa1f339cb77dd8ab9714
Carl Simon Adorf <csadorf@umich.edu>
2019-05-19 14:43:30 -0400

    Drop support for Python 3.4.

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         "Topic :: Database",
         "Topic :: Scientific/Engineering :: Physics",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
