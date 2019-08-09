633433b4d4c124c67a74920599e2604aee89b3e0
Carl Simon Adorf <csadorf@umich.edu>
2019-08-09 10:11:06 -0400

    Reformat init_jobs() doc-string.

--- signac/testing.py
+++ signac/testing.py
@@ -7,17 +7,26 @@
 def init_jobs(project, nested=False, listed=False, heterogeneous=False):
     """Initialize a dataspace for testing purposes
 
-    :param project: The project to add the jobs to
-    :type project: :py:class:`~.project.Project`
-    :param nested: If True, included nested state points
-    :type nested: bool
-    :param listed: If True, include lists as values of state point parameters
-    :type listed: bool
-    :param heterogeneous: If True, include heterogeneous state point parameters
-    :type heterogeneous: bool
-    :returns: The project handle of the initialized project
-    :rtype: :py:class:`~.Project`
-
+    :param project:
+        The project to add the jobs to
+    :type project:
+        :py:class:`~.project.Project`
+    :param nested:
+        If True, included nested state points
+    :type nested:
+        bool
+    :param listed:
+        If True, include lists as values of state point parameters
+    :type listed:
+        bool
+    :param heterogeneous:
+        If True, include heterogeneous state point parameters
+    :type heterogeneous:
+        bool
+    :returns:
+        The project handle of the initialized project
+    :rtype:
+        :py:class:`~.Project`
     """
     vals = [1, 1.0, '1', True, None]
     if nested:
