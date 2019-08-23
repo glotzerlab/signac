7d0c2b53e9a5050a2d7d14aa95461642f4654635
Tommy Waltmann <tomwalt@umich.edu>
2019-08-23 12:58:24 -0400

    fixed some indentations

--- signac/contrib/utility.py
+++ signac/contrib/utility.py
@@ -107,7 +107,7 @@ def add_verbosity_action_argument(parser, default=0):
 
 @deprecated(deprecated_in="1.3", removed_in="2.0", current_version=__version__,
             details=("No references to this function by things outside the module, "
-            "and the things that reference this also aren't used outside the module."))
+                     "and the things that reference this also aren't used outside the module."))
 def set_verbosity_level(verbosity, default=None, increment=10):
     """Set the verbosity level as a function of an integer level.
 
@@ -126,7 +126,7 @@ class VerbosityAction(argparse.Action):
 
     @deprecated(deprecated_in="1.3", removed_in="2.0", current_version=__version__,
                 details=("No references to this function by things outside the module, "
-                "and the things that reference this also aren't used outside the module."))
+                         "and the things that reference this also aren't used outside the module."))
     def __call__(self, parser, args, values, option_string=None):
         if values is None:
             values = '1'
