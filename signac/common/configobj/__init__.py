b2dbe74590246867f39765fd4badb59275a6a161
Bradley Dice <bdice@bradleydice.com>
2019-10-09 16:31:53 -0400

    Add Michigan copyright for modified code.

--- signac/common/configobj/__init__.py
+++ signac/common/configobj/__init__.py
@@ -4,12 +4,13 @@
 
 """A config file reader/writer that supports nested sections in config files."""
 
-# Copyright (C) 2005-2014:
+# Copyright (C) 2005-2019:
 # (name) : (email)
 # Michael Foord: fuzzyman AT voidspace DOT org DOT uk
 # Nicola Larosa: nico AT tekNico DOT net
 # Rob Dennis: rdennis AT gmail DOT com
 # Eli Courtwright: eli AT courtwright DOT org
+# The Regents of the University of Michigan
 
 # This software is licensed under the terms of the BSD license.
 # http://opensource.org/licenses/BSD-3-Clause
