d5213b7c9635c6b7ac09b8c2434cb3ed4a4555cd
Bradley Dice <bdice@bradleydice.com>
2019-12-12 21:53:47 -0500

    Increase docstring info, add example.

--- signac/diff.py
+++ signac/diff.py
@@ -28,9 +28,38 @@ def _dotted_keys_to_nested_dicts(mapping):
 
 
 def diff_jobs(*jobs):
-    """Find differences among a list of jobs' state points.
+    r"""Find differences among a list of jobs' state points.
 
-        :param jobs: One or more jobs whose state points will be diffed.
+    The resulting diff is a dictionary where the keys are job ids and the
+    values are each job's state point minus the intersection of all provided
+    jobs' state points. The comparison is performed over the combined set
+    of keys and values.
+
+    Example:
+
+    >>> import signac
+    >>> project = signac.init_project('project_name')
+    >>> job1 = project.open_job({'constant': 42, 'diff1': 0, 'diff2': 1}).init()
+    >>> job2 = project.open_job({'constant': 42, 'diff1': 1, 'diff2': 1}).init()
+    >>> job3 = project.open_job({'constant': 42, 'diff1': 2, 'diff2': 2}).init()
+    >>> print(job1)
+    c4af2b26f1fd256d70799ad3ce3bdad0
+    >>> print(job2)
+    b96b21fada698f8934d58359c72755c0
+    >>> print(job3)
+    e4289419d2b0e57e4852d44a09f167c0
+    >>> signac.diff_jobs(job1, job2, job3)
+    {'c4af2b26f1fd256d70799ad3ce3bdad0': {'diff2': 1, 'diff1': 0},
+     'b96b21fada698f8934d58359c72755c0': {'diff2': 1, 'diff1': 1},
+     'e4289419d2b0e57e4852d44a09f167c0': {'diff2': 2, 'diff1': 2}}
+    >>> signac.diff_jobs(*project)
+    {'c4af2b26f1fd256d70799ad3ce3bdad0': {'diff2': 1, 'diff1': 0},
+     'b96b21fada698f8934d58359c72755c0': {'diff2': 1, 'diff1': 1},
+     'e4289419d2b0e57e4852d44a09f167c0': {'diff2': 2, 'diff1': 2}}
+
+    :param \*jobs: The jobs whose state points will be diffed.
+    :returns: A dictionary where the keys are job ids and values are the unique
+        parts of that job's state point.
     """
     if len(jobs) == 0:
         return {}
