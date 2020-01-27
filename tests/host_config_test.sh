b9c86383120a20a8fcdb8802f2935233a25f30bc
unknown <vishavsharma1771@gmail.com>
2020-01-27 16:34:02 +0530

    Minor Changes

--- tests/host_config_test.sh
+++ tests/host_config_test.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 # Run host configuration tests.
 
-SIGNAC = "signac $@"
-HOSTNAME = testcfg
+SIGNAC="signac $@"
+HOSTNAME=testcfg
 
-set - e
+set -e
 
-CURRENT_URI =`signac config show hosts.$HOSTNAME.url`
-CURRENT_USERNAME =`signac config show hosts.$HOSTNAME.username`
-CURRENT_PW =`signac config host $HOSTNAME - -show - pw`
+CURRENT_URI=`signac config show hosts.$HOSTNAME.url`
+CURRENT_USERNAME=`signac config show hosts.$HOSTNAME.username`
+CURRENT_PW=`signac config host $HOSTNAME --show-pw`
 
-python . / connection_test.py
-$SIGNAC - -yes config host $HOSTNAME - r
-$SIGNAC - -yes config host $HOSTNAME $CURRENT_URI
-$SIGNAC - -yes config host $HOSTNAME - r
-$SIGNAC - -yes config host $HOSTNAME $CURRENT_URI - u $CURRENT_USERNAME
-$SIGNAC - -yes config host $HOSTNAME - r
-$SIGNAC - -yes config host $HOSTNAME $CURRENT_URI - u $CURRENT_USERNAME - p $CURRENT_PW
-python . / connection_test.py
-$SIGNAC - -yes config host $HOSTNAME - -update - pw None - p $CURRENT_PW
-python . / connection_test.py
-$SIGNAC - -yes config host $HOSTNAME - r
-$SIGNAC - -yes config host $HOSTNAME $CURRENT_URI - u $CURRENT_USERNAME - p $CURRENT_PW
-python . / connection_test.py
+python ./connection_test.py
+$SIGNAC --yes config host $HOSTNAME -r
+$SIGNAC --yes config host $HOSTNAME $CURRENT_URI
+$SIGNAC --yes config host $HOSTNAME -r
+$SIGNAC --yes config host $HOSTNAME $CURRENT_URI -u $CURRENT_USERNAME
+$SIGNAC --yes config host $HOSTNAME -r
+$SIGNAC --yes config host $HOSTNAME $CURRENT_URI -u $CURRENT_USERNAME -p $CURRENT_PW
+python ./connection_test.py
+$SIGNAC --yes config host $HOSTNAME --update-pw None -p $CURRENT_PW
+python ./connection_test.py
+$SIGNAC --yes config host $HOSTNAME -r
+$SIGNAC --yes config host $HOSTNAME $CURRENT_URI -u $CURRENT_USERNAME -p $CURRENT_PW
+python ./connection_test.py
