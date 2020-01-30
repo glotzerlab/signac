819766f13c36896bd83125c04d18fa9377623b80
unknown <vishavsharma1771@gmail.com>
2020-01-30 18:55:12 +0530

    removal of a deprecated method get_statepoint

--- signac/contrib/project.py
+++ signac/contrib/project.py
@@ -448,7 +448,7 @@ class Project(object):
                     id = matches[0]
                 elif len(matches) > 1:
                     raise LookupError(id)
-            return self.Job(project=self, statepoint=self.get_statepoint(id), _id=id)
+            return self.Job(project=self, statepoint=self.open_job(id).sp, _id=id)
 
     def _job_dirs(self):
         try:
@@ -778,7 +778,7 @@ class Project(object):
             tmp = dict()
         if statepoints is None:
             job_ids = self._job_dirs()
-            _cache = {_id: self.get_statepoint(_id) for _id in job_ids}
+            _cache = {_id: self.open_job(_id).sp for _id in job_ids}
         else:
             _cache = {calc_id(sp): sp for sp in statepoints}
 
@@ -1171,7 +1171,7 @@ class Project(object):
         logger.info("Checking workspace for corruption...")
         for job_id in self._find_job_ids():
             try:
-                sp = self.get_statepoint(job_id)
+                sp = self.open_job(job_id).sp
                 if calc_id(sp) != job_id:
                     corrupted.append(job_id)
                 else:
@@ -1220,7 +1220,7 @@ class Project(object):
         for job_id in job_ids:
             try:
                 # First, check if we can look up the state point.
-                sp = self.get_statepoint(job_id)
+                sp = self.open_job(job_id).sp
                 # Check if state point and id correspond.
                 correct_id = calc_id(sp)
                 if correct_id != job_id:
@@ -1272,7 +1272,7 @@ class Project(object):
         for _id in to_remove:
             del self._index_cache[_id]
         for _id in to_add:
-            self._index_cache[_id] = dict(statepoint=self.get_statepoint(_id), _id=_id)
+            self._index_cache[_id] = dict(statepoint=self.open_job(_id).sp, _id=_id)
         return self._index_cache.values()
 
     def _build_index(self, include_job_document=False):
@@ -1281,7 +1281,7 @@ class Project(object):
         """
         wd = self.workspace() if self.Job is Job else None
         for _id in self._find_job_ids():
-            doc = dict(_id=_id, statepoint=self.get_statepoint(_id))
+            doc = dict(_id=_id, statepoint=self.open_job(_id).sp)
             if include_job_document:
                 if wd is None:
                     doc.update(self.open_job(id=_id).document)
