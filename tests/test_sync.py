4398d856d883d93fe7488c5a22a9a4708c0a7a6e
unknown <vishavsharma1771@gmail.com>
2020-02-01 03:07:28 +0530

    Removed tests

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -592,9 +592,7 @@ class ProjectSyncTest():
             self.project_a.sync(self.project_b)
         self.project_a.sync(self.project_b, doc_sync=sync.DocSync.ByKey('b'))
 
-        
-
-    def _setup_mixed_same_data_type(self):
+    def _setup_mixed(self):
         for i in range(4):
             if i % 2 == 0:
                 self._init_job(self.project_a.open_job({'a': i}))
@@ -614,12 +612,6 @@ class ProjectSyncTest():
         for i in range(4):
             self._init_job(self.project_a.open_job({'a': i}))
             self._init_job(self.project_b.open_job({'a': i}))
-
-    def test_same(self):
-        self._setup_jobs()
-        self.project_a.sync(self.project_b, check_schema=False)
-        self.assertEqual(len(self.project_a), 4)
-
 
     def test_with_conflict(self):
         self._setup_jobs()
