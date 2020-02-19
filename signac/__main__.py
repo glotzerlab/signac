9f212d85021091d84775eb7cf7153ed7983322cb
Abhavya Chandra <abhavya.chandra@iitgn.ac.in>
2020-02-19 22:39:05 +0530

    check conditions for .signac_shell_history permission. (#279)
    * check conditions for .signac_shell_history permission
    
    * changing to try-except stype
    
    * correcting the print statement.
    
    * suggested changes
    
    * Revert "suggested changes"
    
    This reverts commit a308fd5f2cfc29d1ee53ad406acb3aa992c68e95.
    
    *  suggested chagnes.
    
    * suggested changes related to white-space.
    
    Co-Authored-By: Bradley Dice <bdice@bradleydice.com>
    
    * removing unwanted line
    
    * correcting the code style
    
    * changing write(w) to append(a)
    
    * adding suggested changes
    
    * Update changelog.txt
    
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>
    Co-authored-by: Carl Simon Adorf <carl.simon.adorf@gmail.com>
    Co-authored-by: Alyssa Travitz <31974495+atravitz@users.noreply.github.com>

--- signac/__main__.py
+++ signac/__main__.py
@@ -1052,11 +1052,19 @@ def main_shell(args):
                     try:
                         readline.read_history_file(fn_hist)
                         readline.set_history_length(1000)
-                    except (IOError, OSError) as error:
-                        if error.errno != errno.ENOENT:
-                            raise
-                    atexit.register(readline.write_history_file, fn_hist)
+                    except FileNotFoundError:
+                        pass
+                    except PermissionError:
+                        print("Warning: Shell history could not be read from "
+                              "{}.".format(os.path.relpath(fn_hist)))
 
+                    def write_history_file():
+                        try:
+                            readline.write_history_file(fn_hist)
+                        except PermissionError:
+                            print("Warning: Shell history could not be written to "
+                                  "{}.".format(os.path.relpath(fn_hist)))
+                    atexit.register(write_history_file)
                 readline.set_completer(Completer(local_ns).complete)
                 readline.parse_and_bind('tab: complete')
             code.interact(
