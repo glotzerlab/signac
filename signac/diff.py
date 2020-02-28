db5c07152f907acc14b06402c4bce8f514443de6
unknown <vishavsharma1771@gmail.com>
2020-02-28 23:29:02 +0530

    Error

--- signac/diff.py
+++ signac/diff.py
@@ -56,6 +56,6 @@ def diff_jobs(*jobs):
         diffs = {}
         for job in jobs:
             unique_sps = sps[job]-intersection
-            diffs[job.id] = _dotted_keys_to_nested_dicts(dict(unique_sps))
+            diffs[job.id] = _dotted_dict_to_nested_dicts(dict(unique_sps))
 
         return diffs
