87a01433d8ed297e3405b400156d40a1cc18f11c
Bradley Dice <bdice@bradleydice.com>
2020-04-09 14:23:43 -0400

    Update hashing.py for pydocstyle.

--- signac/contrib/hashing.py
+++ signac/contrib/hashing.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2017 The Regents of the University of Michigan
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
+"""Hashing functions."""
+
 import hashlib
 # we need to use the standard module here to ensure
 # exact consistent formatting
@@ -8,16 +10,17 @@ import json
 
 
 def calc_id(spec):
-    """Calculate and return a hash value for the given spec."
+    """Calculate and return a hash value for the given spec.
 
     Parameters
     ----------
     spec :
+        Data to hash.
 
     Returns
     -------
     str
-        encoded hash in hexadecimal format.
+        Encoded hash in hexadecimal format.
 
     """
     blob = json.dumps(spec, sort_keys=True)
