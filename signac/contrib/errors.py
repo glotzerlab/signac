03387c0281933ae135c3e88d834b9de5b8cd4c7a
ac-optimus <abhavya.chandra@iitgn.ac.in>
2020-04-16 14:39:33 +0530

    adding newline after the file description

--- signac/contrib/errors.py
+++ signac/contrib/errors.py
@@ -2,6 +2,7 @@
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
 """Errors raised by signac.contrib classes."""
+
 from ..core.errors import Error
 
 
--- signac/contrib/filterparse.py
+++ signac/contrib/filterparse.py
@@ -2,6 +2,7 @@
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
 """Parse the filter arguments."""
+
 import sys
 from ..core import json
 
@@ -87,7 +88,6 @@ def _is_regex(q):
     ----------
     q : str
         Query string.
-
 
     Returns
     -------
