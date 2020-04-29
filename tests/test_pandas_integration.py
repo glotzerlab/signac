c16413c6cfbfc81d4a905de7e15006a72b1557cc
Bradley Dice <bdice@bradleydice.com>
2020-04-28 20:18:53 -0500

    Add tests for includes/excludes.

--- tests/test_pandas_integration.py
+++ tests/test_pandas_integration.py
@@ -35,3 +35,59 @@ class TestPandasIntegration(TestProjectBase):
         assert len(df) == len(jobs)
         assert 'sp.a' in df.columns
         assert 'doc.b' in df.columns
+
+    def test_prefixes(self):
+        for i in range(10):
+            job = self.project.open_job(dict(a=i))
+            job.doc.b = float(i)
+        df = self.project.to_dataframe(sp_prefix='', doc_prefix='')
+        assert len(df) == len(self.project)
+        assert 'a' in df.columns
+        assert 'b' in df.columns
+
+    def test_includes_excludes(self):
+        for i in range(10):
+            job = self.project.open_job(dict(a=i, b=i*2))
+            job.doc.c = float(i)
+            job.doc.d = float(i*3)
+
+        # Including no keys should return an empty DataFrame
+        df = self.project.to_dataframe(sp_includes=[], doc_includes=[])
+        assert len(df) == 0
+
+        # Excluding all keys should return an empty DataFrame
+        df = self.project.to_dataframe(
+            sp_excludes=['a', 'b'], doc_excludes=['c', 'd'])
+        assert len(df) == 0
+
+        # Include one state point column
+        df = self.project.to_dataframe(sp_includes=['a'])
+        assert 'sp.a' in df.columns
+        assert 'sp.b' not in df.columns
+        assert 'doc.c' in df.columns
+        assert 'doc.d' in df.columns
+        assert len(df) == len(self.project)
+
+        # Exclude one state point column
+        df = self.project.to_dataframe(sp_excludes=['b'])
+        assert 'sp.a' in df.columns
+        assert 'sp.b' not in df.columns
+        assert 'doc.c' in df.columns
+        assert 'doc.d' in df.columns
+        assert len(df) == len(self.project)
+
+        # Include one document column
+        df = self.project.to_dataframe(doc_includes=['c'])
+        assert 'sp.a' in df.columns
+        assert 'sp.b' in df.columns
+        assert 'doc.c' in df.columns
+        assert 'doc.d' not in df.columns
+        assert len(df) == len(self.project)
+
+        # Exclude one document column
+        df = self.project.to_dataframe(doc_excludes=['d'])
+        assert 'sp.a' in df.columns
+        assert 'sp.b' in df.columns
+        assert 'doc.c' in df.columns
+        assert 'doc.d' not in df.columns
+        assert len(df) == len(self.project)
