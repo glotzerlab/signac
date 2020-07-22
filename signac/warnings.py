3c1f78b03dc23a2460fa139af8e442096ad63676
Mike Henry <mikehenry@boisestate.edu>
2020-07-22 16:10:51 -0600

    Update signac/warnings.py
    Co-authored-by: Alyssa Travitz <31974495+atravitz@users.noreply.github.com>

--- signac/warnings.py
+++ signac/warnings.py
@@ -3,7 +3,7 @@
 # This software is licensed under the BSD 3-Clause License.
 
 
-"""Module for signac depercation warnings."""
+"""Module for signac deprecation warnings."""
 
 
 class SignacDeprecationWarning(UserWarning):
