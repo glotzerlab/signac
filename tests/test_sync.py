f033246754e1d0519a3561116e4105f05c6b9777
Alyssa Travitz <atravitz@umich.edu>
2020-10-30 09:40:00 -0400

    replacing find_job_ids() in test_sync.py

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -648,11 +648,15 @@ class TestProjectSync():
         with pytest.raises(FileSyncConflict):
             self.project_a.sync(self.project_b, selection=self.project_b.find_jobs({'a': 0}))
         with pytest.raises(FileSyncConflict):
-            self.project_a.sync(self.project_b, selection=self.project_a.find_job_ids({'a': 0}))
+            self.project_a.sync(self.project_b,
+                                selection=[job.id for job in self.project_a.find_jobs()])
         with pytest.raises(FileSyncConflict):
-            self.project_a.sync(self.project_b, selection=self.project_b.find_job_ids({'a': 0}))
+            self.project_a.sync(self.project_b,
+                                selection=[job.id for job in self.project_b.find_jobs()])
         f = {'a': {'$ne': 0}}
         self.project_a.sync(self.project_b, selection=self.project_a.find_jobs(f))
         self.project_a.sync(self.project_b, selection=self.project_b.find_jobs(f))
-        self.project_a.sync(self.project_b, selection=self.project_a.find_job_ids(f))
-        self.project_a.sync(self.project_b, selection=self.project_b.find_job_ids(f))
+        self.project_a.sync(self.project_b,
+                            selection=[job.id for job in self.project_a.find_jobs(f)])
+        self.project_a.sync(self.project_b,
+                            selection=[job.id for job in self.project_b.find_jobs(f)])
