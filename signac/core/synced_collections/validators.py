427533993cecfcf49d2dbab4b8839fcdceaeb454
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2020-12-28 12:48:49 -0800

    Change validators to use new class.

--- signac/core/synced_collections/validators.py
+++ signac/core/synced_collections/validators.py
@@ -11,9 +11,9 @@ types as needed.
 """
 
 from collections.abc import Mapping, Sequence
-from enum import Enum, auto
 
 from ...errors import InvalidKeyError, KeyTypeError
+from .utils import AbstractTypeResolver
 
 try:
     import numpy
@@ -23,26 +23,13 @@ except ImportError:
     NUMPY = False
 
 
-"""Instance checks for abcs are expensive. Moreover, even for base classes isinstance
-is slower than a dict lookup. Therefore, we can be much faster by only calling isinstance
-when we have not seen a type before, then caching it; it's very unlikely that a user
-will be using many _different_ data types in a given run."""
-
-
-class AbstractTypes(Enum):
-    """Simple representation of ABCs to avoid instance checks."""
-
-    GENERIC = auto()
-    MAPPING = auto()
-    NON_STR_SEQUENCE = auto()
-
-
-_TYPE_CACHE = {
-    str: AbstractTypes.GENERIC,
-    dict: AbstractTypes.MAPPING,
-    list: AbstractTypes.NON_STR_SEQUENCE,
-    tuple: AbstractTypes.NON_STR_SEQUENCE,
-}
+_no_dot_in_key_type_resolver = AbstractTypeResolver(
+    {
+        "MAPPING": lambda obj: isinstance(obj, Mapping),
+        "NON_STR_SEQUENCE": lambda obj: isinstance(obj, Sequence)
+        and not isinstance(obj, str),
+    }
+)
 
 
 def no_dot_in_key(data):
@@ -63,18 +50,9 @@ def no_dot_in_key(data):
     """
     VALID_KEY_TYPES = (str, int, bool, type(None))
 
-    dtype = type(data)
-    try:
-        switch_type = _TYPE_CACHE[dtype]
-    except KeyError:
-        if isinstance(data, Mapping):
-            switch_type = _TYPE_CACHE[dtype] = AbstractTypes.MAPPING
-        elif isinstance(data, Sequence) and not isinstance(data, str):
-            switch_type = _TYPE_CACHE[dtype] = AbstractTypes.NON_STR_SEQUENCE
-        else:
-            switch_type = _TYPE_CACHE[dtype] = AbstractTypes.GENERIC
+    switch_type = _no_dot_in_key_type_resolver.get_type(data)
 
-    if switch_type == AbstractTypes.MAPPING:
+    if switch_type == "MAPPING":
         for key, value in data.items():
             if isinstance(key, str):
                 if "." in key:
@@ -86,31 +64,19 @@ def no_dot_in_key(data):
                     f"Mapping keys must be str, int, bool or None, not {type(key).__name__}"
                 )
             no_dot_in_key(value)
-    elif switch_type == AbstractTypes.NON_STR_SEQUENCE:
+    elif switch_type == "NON_STR_SEQUENCE":
         for value in data:
             no_dot_in_key(value)
 
 
-class AbstractTypesJSON(Enum):
-    """Simple representation of ABCs to avoid instance checks."""
-
-    BASE = auto()
-    MAPPING = auto()
-    SEQUENCE = auto()
-    NUMPY = auto()
-    INVALID = auto()
-
-
-_TYPE_CACHE_JSON = {
-    str: AbstractTypesJSON.BASE,
-    int: AbstractTypesJSON.BASE,
-    float: AbstractTypesJSON.BASE,
-    bool: AbstractTypesJSON.BASE,
-    type(None): AbstractTypesJSON.BASE,
-    dict: AbstractTypesJSON.MAPPING,
-    list: AbstractTypesJSON.SEQUENCE,
-    tuple: AbstractTypesJSON.SEQUENCE,
-}
+_json_format_validator_type_resolver = AbstractTypeResolver(
+    {
+        "BASE": lambda obj: isinstance(obj, (str, int, float, bool, type(None))),
+        "MAPPING": lambda obj: isinstance(obj, Mapping),
+        "SEQUENCE": lambda obj: isinstance(obj, Sequence),
+        "NUMPY": lambda obj: NUMPY and isinstance(obj, (numpy.ndarray, numpy.number)),
+    }
+)
 
 
 def json_format_validator(data):
@@ -129,25 +95,11 @@ def json_format_validator(data):
         If the data type of ``data`` is not supported.
 
     """
-    dtype = type(data)
+    switch_type = _json_format_validator_type_resolver.get_type(data)
 
-    try:
-        switch_type = _TYPE_CACHE_JSON[dtype]
-    except KeyError:
-        if isinstance(data, (str, int, float, bool, type(None))):
-            switch_type = _TYPE_CACHE_JSON[dtype] = AbstractTypesJSON.BASE
-        if isinstance(data, Mapping):
-            switch_type = _TYPE_CACHE_JSON[dtype] = AbstractTypesJSON.MAPPING
-        elif isinstance(data, Sequence):
-            switch_type = _TYPE_CACHE_JSON[dtype] = AbstractTypesJSON.SEQUENCE
-        elif NUMPY and isinstance(data, (numpy.ndarray, numpy.number)):
-            switch_type = _TYPE_CACHE_JSON[dtype] = AbstractTypesJSON.NUMPY
-        else:
-            switch_type = _TYPE_CACHE_JSON[dtype] = AbstractTypesJSON.INVALID
-
-    if switch_type == AbstractTypesJSON.BASE:
+    if switch_type == "BASE":
         return
-    elif switch_type == AbstractTypesJSON.MAPPING:
+    elif switch_type == "MAPPING":
         for key, value in data.items():
             # Support for non-str keys will be removed in version 2.0.
             # See issue: https://github.com/glotzerlab/signac/issues/316.
@@ -156,10 +108,10 @@ def json_format_validator(data):
                     f"Keys must be str, int, bool or None, not {type(key).__name__}"
                 )
             json_format_validator(value)
-    elif switch_type == AbstractTypesJSON.SEQUENCE:
+    elif switch_type == "SEQUENCE":
         for value in data:
             json_format_validator(value)
-    elif switch_type == AbstractTypesJSON.NUMPY:
+    elif switch_type == "NUMPY":
         if numpy.iscomplex(data).any():
             raise TypeError(
                 "NumPy object with complex value(s) is not JSON serializable"
