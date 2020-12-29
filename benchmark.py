3232fc5b26b418b64e076345e410a08c396fc885
Bradley Dice <bdice@bradleydice.com>
2020-12-28 20:06:45 -0600

    Revert benchmark id to get_id() for backwards compatibility.

--- benchmark.py
+++ benchmark.py
@@ -204,7 +204,7 @@ def benchmark_project(project, keys=None):
         "select_by_id",
         Timer(
             stmt="project.open_job(id=job_id)",
-            setup=setup + "job_id = random.choice(list(islice(project, 100))).id",
+            setup=setup + "job_id = random.choice(list(islice(project, 100))).get_id()",
         ),
     )
 
