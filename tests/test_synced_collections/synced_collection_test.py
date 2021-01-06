30f18e3f0fa6e0d66c006d1719ee361d4165bd02
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2021-01-05 17:13:46 -0800

    Skip expected failures of multithreaded execution in single-threaded mode on PyPy.

--- tests/test_synced_collections/synced_collection_test.py
+++ tests/test_synced_collections/synced_collection_test.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2020 The Regents of the University of Michigan
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
+import platform
 from collections.abc import MutableMapping, MutableSequence
 from copy import deepcopy
 
@@ -15,6 +16,9 @@ try:
     NUMPY = True
 except ImportError:
     NUMPY = False
+
+
+PYPY = "PyPy" in platform.python_implementation()
 
 
 class SyncedCollectionTest:
@@ -415,25 +419,29 @@ class SyncedDictTest(SyncedCollectionTest):
 
         assert len(synced_collection) == num_threads
 
-        # Now clear the data and try again with multithreading disabled. Unless
-        # we're very unlucky, some of these threads should overwrite each
-        # other.
-        type(synced_collection).disable_multithreading()
-        synced_collection.clear()
+        # The ways that PyPy fails here due to what appears to be optimization
+        # of the json library under the hood, so we skip testing the failure
+        # cases on PyPy.
+        if not PYPY:
+            # Now clear the data and try again with multithreading disabled. Unless
+            # we're very unlucky, some of these threads should overwrite each
+            # other.
+            type(synced_collection).disable_multithreading()
+            synced_collection.clear()
 
-        with ThreadPoolExecutor(max_workers=num_threads) as executor:
-            list(executor.map(set_value, [synced_collection] * num_threads * 10))
+            with ThreadPoolExecutor(max_workers=num_threads) as executor:
+                list(executor.map(set_value, [synced_collection] * num_threads * 10))
 
-        assert len(synced_collection) != num_threads
+            assert len(synced_collection) != num_threads
 
-        # For good measure, try reenabling multithreading and test to be safe.
-        type(synced_collection).enable_multithreading()
-        synced_collection.clear()
+            # For good measure, try reenabling multithreading and test to be safe.
+            type(synced_collection).enable_multithreading()
+            synced_collection.clear()
 
-        with ThreadPoolExecutor(max_workers=num_threads) as executor:
-            list(executor.map(set_value, [synced_collection] * num_threads * 10))
+            with ThreadPoolExecutor(max_workers=num_threads) as executor:
+                list(executor.map(set_value, [synced_collection] * num_threads * 10))
 
-        assert len(synced_collection) == num_threads
+            assert len(synced_collection) == num_threads
 
 
 class SyncedListTest(SyncedCollectionTest):
