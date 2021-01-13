a1ddee76e73c679d73a67bbc9eee82045933e9a8
Shantanu Dave <shantanu.dave95@gmail.com>
2021-01-13 22:12:07 +0530

    Replaced syncutil.copytree with shutil.copytree

--- signac/contrib/project.py
+++ signac/contrib/project.py
@@ -1494,7 +1494,7 @@ class Project:
         """
         job.update_statepoint(update=update, overwrite=overwrite)
 
-    def clone(self, job, copytree=syncutil.copytree):
+    def clone(self, job, copytree=shutil.copytree):
         """Clone job into this project.
 
         Create an identical copy of job within this project.
@@ -1506,7 +1506,7 @@ class Project:
         job : :class:`~signac.contrib.job.Job`
             The job to copy into this project.
         copytree :
-             (Default value = syncutil.copytree)
+             (Default value = shutil.copytree)
 
         Returns
         -------
