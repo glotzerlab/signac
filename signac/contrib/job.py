58e29e88044a9b0f31bcd6c6df2262f85e81138f
Bradley Dice <bdice@bradleydice.com>
2021-02-19 16:03:11 -0600

    Skip validation if JSON is valid.

--- signac/contrib/job.py
+++ signac/contrib/job.py
@@ -215,7 +215,7 @@ class _StatePointDict(JSONAttrDict):
             raise JobsCorruptedError([job_id])
 
         with self._suspend_sync:
-            self._update(data)
+            self._update(data, trust_source=True)
 
         return data
 
