a0d643d731db9ac550e51d195eb415b58cfe5d0b
Bradley Dice <bdice@bradleydice.com>
2021-07-30 14:07:09 -0500

    Remove database and host components. (#576)
    * Remove database and host components.
    
    * Update docs/setup files.
    
    * Remove utility function.

--- signac/core/utility.py
+++ signac/core/utility.py
@@ -4,41 +4,6 @@
 """Utility classes for signac version."""
 
 import re
-import subprocess
-
-from deprecation import deprecated
-
-from ..version import __version__
-
-
-@deprecated(
-    deprecated_in="1.3",
-    removed_in="2.0",
-    current_version=__version__,
-    details="All database related functions have been deprecated.",
-)
-def get_subject_from_certificate(fn_certificate):  # noqa: D103, E261
-    try:
-        cert_txt = subprocess.check_output(
-            [
-                "openssl",
-                "x509",
-                "-in",
-                fn_certificate,
-                "-inform",
-                "PEM",
-                "-subject",
-                "-nameopt",
-                "RFC2253",
-            ]
-        ).decode()
-    except subprocess.CalledProcessError:
-        msg = "Unable to retrieve subject from certificate '{}'."
-        raise RuntimeError(msg.format(fn_certificate))
-    else:
-        lines = cert_txt.split("\n")
-        assert lines[0].startswith("subject=")
-        return lines[0][len("subject=") :].strip()
 
 
 class Version(dict):
