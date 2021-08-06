54770dc1a175a3448f06d72acab6e7fd03651c1d
Bradley Dice <bdice@bradleydice.com>
2021-08-06 11:22:17 -0500

    Clean up utility functions. (#597)
    * Clean up utility functions.
    
    * Add missing test case for dotted keys function where values are empty dicts.

--- signac/contrib/utility.py
+++ signac/contrib/utility.py
@@ -6,18 +6,14 @@
 import logging
 import os
 import sys
-import tarfile
-import zipfile
 from collections.abc import Mapping
-from contextlib import contextmanager
 from datetime import timedelta
-from tempfile import TemporaryDirectory
 from time import time
 
 logger = logging.getLogger(__name__)
 
 
-def query_yes_no(question, default="yes"):
+def query_yes_no(question, default="yes"):  # pragma: no cover
     """Ask a yes/no question via input() and return their answer.
 
     "question" is a string that is presented to the user.
@@ -150,7 +146,7 @@ def _mkdir_p(path):
         os.makedirs(path, exist_ok=True)
 
 
-def split_and_print_progress(iterable, num_chunks=10, write=None, desc="Progress: "):
+def _split_and_print_progress(iterable, num_chunks=10, write=None, desc="Progress: "):
     """Split the progress and prints it.
 
     Parameters
@@ -199,39 +195,6 @@ def split_and_print_progress(iterable, num_chunks=10, write=None, desc="Progress
         write(f"{desc}100%")
     else:
         yield iterable
-
-
-@contextmanager
-def _extract(filename):
-    """Extract zipfile and tarfile.
-
-    Parameters
-    ----------
-    filename : str
-        Name of zipfile/tarfile to extract.
-
-    Yields
-    ------
-    str
-        Path to the extracted directory.
-
-    Raises
-    ------
-    RuntimeError
-        When the provided file is neither a zipfile nor a tarfile.
-
-    """
-    with TemporaryDirectory() as tmpdir:
-        if zipfile.is_zipfile(filename):
-            with zipfile.ZipFile(filename) as file:
-                file.extractall(tmpdir)
-                yield tmpdir
-        elif tarfile.is_tarfile(filename):
-            with tarfile.open(filename) as file:
-                file.extractall(path=tmpdir)
-                yield tmpdir
-        else:
-            raise RuntimeError(f"Unknown file type: '{filename}'.")
 
 
 def _dotted_dict_to_nested_dicts(dotted_dict, delimiter_nested="."):
