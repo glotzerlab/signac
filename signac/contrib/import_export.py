5641b0f4c2b3014b6076931cccdb489ec5134967
Bradley Dice <bdice@bradleydice.com>
2021-08-07 14:44:14 -0500

    Use generator in the same line it is constructed; remove unclear variable name jsi.

--- signac/contrib/import_export.py
+++ signac/contrib/import_export.py
@@ -63,8 +63,7 @@ def _make_schema_based_path_function(jobs, exclude_keys=None, delimiter_nested="
         return lambda job, sep=None: ""
 
     index = [{"_id": job.id, "sp": job.sp()} for job in jobs]
-    jsi = _build_job_statepoint_index(exclude_const=True, index=index)
-    sp_index = OrderedDict(jsi)
+    sp_index = OrderedDict(_build_job_statepoint_index(exclude_const=True, index=index))
 
     paths = {}
     for key_tokens, values in sp_index.items():
