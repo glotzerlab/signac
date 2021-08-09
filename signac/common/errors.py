b9788280ef72eb0dfcb3644346406d90de2a0178
Bradley Dice <bdice@bradleydice.com>
2021-08-09 09:36:11 -0500

    Remove unused error classes. (#595)
    * Remove unused error classes.
    
    * Remove AuthenticationError, ExportError, FetchError.

--- signac/common/errors.py
+++ signac/common/errors.py
@@ -9,28 +9,3 @@ class ConfigError(Error, RuntimeError):
     """Error with parsing or reading a configuration file."""
 
     pass
-
-
-# this class is only used by deprecated features
-class AuthenticationError(Error, RuntimeError):
-    """Authentication error."""
-
-    def __str__(self):
-        if len(self.args) > 0:
-            return f"Failed to authenticate with host '{self.args[0]}'."
-        else:
-            return "Failed to authenticate with host."
-
-
-# this class is only used by deprecated features
-class ExportError(Error, RuntimeError):
-    """Error exporting documents to a mirror."""
-
-    pass
-
-
-# this class is only used by deprecated features
-class FetchError(FileNotFoundError):
-    """Error in fetching data."""
-
-    pass
