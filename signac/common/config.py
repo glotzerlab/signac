5c02cdc64d21f356410918e1ea61c550f80b483e
Bradley Dice <bdice@bradleydice.com>
2021-08-09 09:42:13 -0500

    Remove hosts validators (#596)
    * Remove hosts sections from config validator.
    
    * Remove password validators and file permission handlers.

--- signac/common/config.py
+++ signac/common/config.py
@@ -5,7 +5,6 @@
 
 import logging
 import os
-import stat
 
 from .configobj import ConfigObj, ConfigObjError
 from .errors import ConfigError
@@ -20,12 +19,6 @@ CONFIG_PATH = [HOME]
 FN_CONFIG = os.path.expanduser("~/.signacrc")
 
 
-class PermissionsError(ConfigError):
-    """Indicates an error in file permissions."""
-
-    pass
-
-
 def _search_local(root):
     for fn in CONFIG_FILENAMES:
         fn_ = os.path.abspath(os.path.join(root, fn))
@@ -33,7 +26,7 @@ def _search_local(root):
             yield fn_
 
 
-def search_tree(root=None):
+def _search_tree(root=None):
     """Locates signac configuration files in a directory hierarchy.
 
     Parameters
@@ -61,37 +54,6 @@ def search_standard_dirs():
         yield from _search_local(path)
 
 
-def check_permissions(filename):
-    """Verify that saved passwords are only readable by the current user."""
-    st = os.stat(filename)
-    if st.st_mode & stat.S_IROTH or st.st_mode & stat.S_IRGRP:
-        raise PermissionsError(
-            "Permissions of configuration file '{fn}'"
-            "allow it to be read by others than the user. "
-            "Unable to read/write password.".format(fn=filename)
-        )
-
-
-def fix_permissions(filename):
-    """Set file permissions to be strictly user-readable and user-writable."""
-    os.chmod(filename, stat.S_IRUSR | stat.S_IWUSR)
-
-
-def check_and_fix_permissions(filename):
-    """Verify file permissions and fix problems if needed."""
-    try:
-        check_permissions(filename)
-    except PermissionsError as permissions_error:
-        logger.debug(f"{permissions_error} Attempting to fix permissions.")
-        try:
-            fix_permissions(filename)
-        except Exception as error:
-            logger.error(f"Failed to fix permissions with error: {error}")
-            raise permissions_error
-        else:
-            logger.debug("Fixed permissions.")
-
-
 def read_config_file(filename):
     """Read a configuration file."""
     logger.debug(f"Reading config file '{filename}'.")
@@ -107,8 +69,6 @@ def read_config_file(filename):
                 os.path.abspath(filename)
             )
         )
-    if config.has_password():
-        check_and_fix_permissions(filename)
     return config
 
 
@@ -134,7 +94,7 @@ def load_config(root=None, local=False):
     else:
         for fn in search_standard_dirs():
             config.merge(read_config_file(fn))
-        for fn in search_tree(root):
+        for fn in _search_tree(root):
             tmp = read_config_file(fn)
             config.merge(tmp)
             if "project" in tmp:
@@ -153,22 +113,3 @@ class Config(ConfigObj):
         if validator is None:
             validator = get_validator()
         return super().validate(validator, *args, **kwargs)
-
-    def has_password(self):
-        """Check if this configuration contains a password."""
-
-        def is_pw(section, key):
-            assert not key.endswith("password")
-
-        try:
-            self.walk(is_pw)
-            return False
-        except AssertionError:
-            return True
-
-    def write(self, outfile=None, section=None):
-        """Write this configuration to a file."""
-        if outfile is not None:
-            if self.has_password():
-                check_and_fix_permissions(outfile)
-        return super().write(outfile=outfile, section=section)
