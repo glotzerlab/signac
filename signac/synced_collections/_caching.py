8499db45578c698b8ab341198f644ef5121faa4a
Bradley Dice <bdice@bradleydice.com>
2021-08-24 09:25:33 -0700

    Avoid AssertionError. (#612)
    * Avoid AssertionError.
    
    * Update changelog.
    
    * Fix redis check.

--- signac/synced_collections/_caching.py
+++ signac/synced_collections/_caching.py
@@ -37,11 +37,12 @@ def get_cache():
             cache = redis.Redis()
             test_key = str(uuid.uuid4())
             cache.set(test_key, 0)
-            assert cache.get(test_key) == b"0"  # Redis stores data as bytes
+            if cache.get(test_key) != b"0":  # Redis stores data as bytes
+                raise RuntimeError("Cache access check failed.")
             cache.delete(test_key)
             logger.info("Using Redis cache.")
             return _RedisCache(cache)
-        except (redis.exceptions.ConnectionError, AssertionError) as error:
+        except (redis.exceptions.ConnectionError, RuntimeError) as error:
             logger.debug(str(error))
     logger.info("Redis not available.")
     return {}
