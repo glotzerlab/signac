77720834a3d5d3d8b16d16dd6a9436a6abfe2a31
Bradley Dice <bdice@bradleydice.com>
2021-08-25 10:38:33 -0700

    Remove doc_filter argument (#586)
    * Remove doc_filter argument from find_jobs, replace with backwards compatible shim.
    
    * Add doc_filter shims to CLI and JobsCursor constructor.
    
    * Add deprecation warning to JobsCursor doc_filter.
    
    * Duplicate tests. Wrap individual function calls in warning filters, rather than the whole test.
    
    * Raise TypeError instead of ValueError.
    
    * Catch deprecation warnings in tests using positional arguments.
    
    * Remove doc_filter from JobsCursor.__init__.
    
    * Replace JobsCursor constructor with a call to find_jobs.
    
    * Add explicit tests of passing None.
    
    * Add tests for explicit filter=None, doc_filter=None, invalid arguments.

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -265,7 +265,7 @@ class TestBasicShell:
                 self.call(
                     "python -m signac find ".split() + ['{"doc.a": ' + str(i) + "}"]
                 ).strip()
-                == [job.id for job in project.find_jobs(doc_filter={"a": i})][0]
+                == [job.id for job in project.find_jobs({"doc.a": i})][0]
             )
 
         for i in range(1, 4):
@@ -273,7 +273,7 @@ class TestBasicShell:
                 self.call(
                     "python -m signac find ".split() + ['{"doc.b": ' + str(i) + "}"]
                 ).strip()
-                == [job.id for job in project.find_jobs(doc_filter={"b": i})][0]
+                == [job.id for job in project.find_jobs({"doc.b": i})][0]
             )
 
     def test_diff(self):
