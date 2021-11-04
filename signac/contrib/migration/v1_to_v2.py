3103deb88b9f430e11ff007bf6e9d220f520c39c
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2021-11-03 23:41:42 -0700

    Add file for new migration.

---
+++ signac/contrib/migration/v1_to_v2.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2021 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+"""Migrate from schema version 1 to version 2.
+
+This migration involves the following changes:
+    - Removal of the signac project id
+"""
+
+
+def migrate_v1_to_v2(project):
+    """Migrate from schema version 1 to version 2."""
+    pass
