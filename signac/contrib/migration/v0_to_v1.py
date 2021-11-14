33de14cf042202fa39cdce207f14e3c282a814d1
Vyas Ramasubramani <vyasr@nvidia.com>
2021-11-14 16:59:01 -0500

    Add isolated functions for handling v1 config loading during migration.

--- signac/contrib/migration/v0_to_v1.py
+++ signac/contrib/migration/v0_to_v1.py
@@ -6,6 +6,39 @@
 This migration is a null-migration that serves as a template
 for future migrations and testing purposes.
 """
+import os
+
+# A minimal v1 config.
+_cfg = """
+schema_version = string()
+"""
+
+
+def _load_config_v1(root_directory):
+    try:
+        import configobj
+    except ModuleNotFoundError:
+        try:
+            from signac.common import configobj
+        except ModuleNotFoundError:
+            raise RuntimeError(
+                "signac schema version 1 can only be read with configobj. "
+                "Please install configobj and try again."
+            )
+    cfg = configobj.ConfigObj(
+        os.path.join(root_directory) + "signac.rc", configspec=_cfg.split("\n")
+    )
+    validator = configobj.validate.Validator()
+    if not cfg.validate(validator):
+        raise RuntimeError(
+            "This project's config file is not compatible with " "signac's v1 schema."
+        )
+    return cfg
+
+
+def _is_schema_version_1(root_directory):
+    cfg = _load_config_v1(root_directory)
+    return cfg["schema_version"] == 1
 
 
 def migrate_v0_to_v1(project):
