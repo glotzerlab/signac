12d7cc7f9d35cd916463c16ca5ec7bd16e52fe6e
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2021-12-11 11:04:45 -0800

    Undo copyright change.

--- signac/contrib/migration/__init__.py
+++ signac/contrib/migration/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2021 The Regents of the University of Michigan
+# Copyright (c) 2019 The Regents of the University of Michigan
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
 """Handle migrations of signac schema versions."""
