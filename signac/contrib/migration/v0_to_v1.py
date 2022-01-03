883d15c4eb3b405dda9bd7a75f06405aa39d2b0c
Vyas Ramasubramani <vyasr@nvidia.com>
2022-01-03 14:51:38 -0800

    Remove backwards compat layer for v0 to v1 migration.

--- signac/contrib/migration/v0_to_v1.py
+++ signac/contrib/migration/v0_to_v1.py
@@ -7,11 +7,6 @@ This migration is a null-migration that serves as a template
 for future migrations and testing purposes.
 """
 import os
-import warnings
-
-from deprecation import deprecated
-
-from ...version import __version__
 
 # A minimal v1 config.
 _cfg = """
@@ -45,24 +40,4 @@ def _load_config_v1(root_directory):
 
 def _migrate_v0_to_v1(root_directory):
     """Migrate from schema version 0 to version 1."""
-    from ..project import Project
-
-    if isinstance(root_directory, Project):
-        warnings.warn(
-            "Migrations should be applied to a directory containing a signac project, "
-            "not a project object.",
-            FutureWarning,
-        )
-        root_directory = root_directory.root_directory()
-    # nothing to do here, serves purely as an example
-
-
-@deprecated(
-    deprecated_in="1.7",
-    removed_in="2.0",
-    current_version=__version__,
-    details=("Migrations should not be invoked directly, only via `apply_migrations`."),
-)
-def migrate_v0_to_v1(root_directory):
-    """Migrate from schema version 0 to version 1."""
-    _migrate_v0_to_v1(root_directory)
+    pass
