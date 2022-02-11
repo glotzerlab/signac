29143aded1897b3732265c81d0bd75eb993cdc84
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2022-02-10 19:51:57 -0800

    Initial migration to schema version 2 including config rename (#678)
    * Implement initial migration to schema version 2.
    
    * Require project-local config to be signac.rc (not .signacrc) and make searches stricter to match.
    
    * Standardize method for getting project config at a root.
    
    * Move config to .signac/config.
    
    * Fix import order.
    
    * Address PR comments.
    
    * Remove some unnecessary code.
    
    * Address final PR coments.

--- signac/common/config.py
+++ signac/common/config.py
@@ -12,18 +12,27 @@ from .validate import cfg, get_validator
 
 logger = logging.getLogger(__name__)
 
-DEFAULT_FILENAME = ".signacrc"
-CONFIG_FILENAMES = [DEFAULT_FILENAME, "signac.rc"]
-HOME = os.path.expanduser("~")
-CONFIG_PATH = [HOME]
-FN_CONFIG = os.path.expanduser("~/.signacrc")
+PROJECT_CONFIG_FN = os.path.join(".signac", "config")
+USER_CONFIG_FN = os.path.expanduser(os.path.join("~", ".signacrc"))
+
+
+def _get_project_config_fn(root):
+    return os.path.abspath(os.path.join(root, PROJECT_CONFIG_FN))
+
+
+def _get_config_dirname(fn):
+    # We could use pathlib's `.paths` attribute to remove the trailing project
+    # config filename, but that has significant performance
+    # implications: this takes ~200 ns, whereas
+    # os.path.join(*(pathlib.PosixPath(fn).parts[:-2])) takes ~6 us, almost a
+    # 20x slowdown.
+    return fn.replace(os.sep + PROJECT_CONFIG_FN, "")
 
 
 def _search_local(root):
-    for fn in CONFIG_FILENAMES:
-        fn_ = os.path.abspath(os.path.join(root, fn))
-        if os.path.isfile(fn_):
-            yield fn_
+    fn_ = _get_project_config_fn(root)
+    if os.path.isfile(fn_):
+        yield fn_
 
 
 def _search_tree(root=None):
@@ -41,8 +50,7 @@ def _search_tree(root=None):
         yield from _search_local(root)
         up = os.path.abspath(os.path.join(root, ".."))
         if up == root:
-            msg = "Reached filesystem root."
-            logger.debug(msg)
+            logger.debug("Reached filesystem root, no config found.")
             return
         else:
             root = up
@@ -50,8 +58,11 @@ def _search_tree(root=None):
 
 def _search_standard_dirs():
     """Locates signac configuration files in standard directories."""
-    for path in CONFIG_PATH:
-        yield from _search_local(path)
+    # For now this search only finds user-specific files, but it could be
+    # updated in the future to support e.g. system-wide config files.
+    for fn in (USER_CONFIG_FN,):
+        if os.path.isfile(fn):
+            yield fn
 
 
 def read_config_file(filename, configspec=None, *args, **kwargs):
@@ -75,10 +86,11 @@ def read_config_file(filename, configspec=None, *args, **kwargs):
     try:
         config = Config(filename, configspec=configspec, *args, **kwargs)
     except (OSError, ConfigObjError) as error:
-        msg = "Failed to read configuration file '{}':\n{}"
-        raise ConfigError(msg.format(filename, error))
+        raise ConfigError(f"Failed to read configuration file '{filename}':\n{error}")
     verification = config.verify()
     if verification is not True:
+        # TODO: In the future this should raise an error, not just a
+        # debug-level logging notice.
         logger.debug(
             "Config file '{}' may contain invalid values.".format(
                 os.path.abspath(filename)
@@ -108,21 +120,24 @@ def load_config(root=None, local=False):
         root = os.getcwd()
     config = Config(configspec=cfg.split("\n"))
     if local:
-        for fn in _search_local(root):
-            tmp = read_config_file(fn)
-            config.merge(tmp)
-            if "project" in tmp:
-                config["project_dir"] = os.path.dirname(fn)
-                break
+        search_func = _search_local
     else:
         for fn in _search_standard_dirs():
             config.merge(read_config_file(fn))
-        for fn in _search_tree(root):
-            tmp = read_config_file(fn)
-            config.merge(tmp)
-            if "project" in tmp:
-                config["project_dir"] = os.path.dirname(fn)
-                break
+        search_func = _search_tree
+
+    for fn in search_func(root):
+        tmp = read_config_file(fn)
+        config.merge(tmp)
+        # Once a valid config file is found, we cease looking any further, i.e.
+        # we assume that the first directory with a valid config file is the
+        # project root.
+        # TODO: Rather than adding the project directory to the config, it
+        # should be returned separately (i.e. the return should become a tuple
+        # (root_dir, config). The current approach confuses the discovery with
+        # the contents of the config.
+        config["project_dir"] = _get_config_dirname(fn)
+        break
     return config
 
 
