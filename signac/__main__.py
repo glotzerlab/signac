1bc218301addd24daa2ea5a843e48d28469c477f
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2022-02-21 01:35:00 -0800

    Remove user-provided project name from API (#677)

--- signac/__main__.py
+++ signac/__main__.py
@@ -354,10 +354,8 @@ def main_view(args):
 
 def main_init(args):
     """Handle init subcommand."""
-    project = init_project(
-        name=args.project_id, root=os.getcwd(), workspace=args.workspace
-    )
-    _print_err(f"Initialized project '{project}'.")
+    init_project(name=args.project_id, root=os.getcwd(), workspace=args.workspace)
+    _print_err("Initialized project.")
 
 
 def main_schema(args):
@@ -948,12 +946,7 @@ def main():
     subparsers = parser.add_subparsers()
 
     parser_init = subparsers.add_parser("init")
-    parser_init.add_argument(
-        "project_id",
-        nargs="?",
-        type=str,
-        help="Initialize a project with the given project id.",
-    )
+    parser_init.add_argument("project_id", nargs="?", help=argparse.SUPPRESS)
     parser_init.add_argument(
         "-w",
         "--workspace",
