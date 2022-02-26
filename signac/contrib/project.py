887577960eab3a9b1dc6a8d1fcb9dd9e95a2bf3b
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2022-02-25 21:42:40 -0800

    Remove project name from schema (#684)
    * Remove project id API.
    
    * Remove project name from config as part of migration.
    
    * Fix issues with config CLI and remove project from default cfg.
    
    * Address PR comments.
    
    * Change the str of a project to the str of its root directory.

--- signac/contrib/project.py
+++ signac/contrib/project.py
@@ -136,17 +136,6 @@ class Project:
         self._config = _ProjectConfig(config)
         self._lock = RLock()
 
-        # Ensure that the project id is configured.
-        try:
-            self._id = str(self.config["project"])
-        except KeyError:
-            raise LookupError(
-                "Unable to determine project id. "
-                "Please verify that '{}' is a signac project path.".format(
-                    os.path.abspath(self.config.get("project_dir", os.getcwd()))
-                )
-            )
-
         # Ensure that the project's data schema is supported.
         self._check_schema_compatibility()
 
@@ -172,8 +161,8 @@ class Project:
                 _mkdir_p(self.workspace)
             except OSError:
                 logger.error(
-                    "Error occurred while trying to create "
-                    "workspace directory for project {}.".format(self.id)
+                    "Error occurred while trying to create workspace directory for project at root "
+                    f"{self.root_directory()}."
                 )
                 raise
 
@@ -191,8 +180,7 @@ class Project:
         )
 
     def __str__(self):
-        """Return the project's id."""
-        return str(self.id)
+        return str(self.root_directory())
 
     def __repr__(self):
         return "{type}.get_project({root})".format(
@@ -210,8 +198,7 @@ class Project:
         """
         return (
             "<p>"
-            + f"<strong>Project:</strong> {self.id}<br>"
-            + f"<strong>Root:</strong> {self.root_directory()}<br>"
+            + f"<strong>Project:</strong> {self.path}<br>"
             + f"<strong>Workspace:</strong> {self.workspace}<br>"
             + f"<strong>Size:</strong> {len(self)}"
             + "</p>"
@@ -269,18 +256,6 @@ class Project:
         See :ref:`signac project -w <signac-cli-project>` for the command line equivalent.
         """
         return self._workspace
-
-    @property
-    def id(self):
-        """Get the project identifier.
-
-        Returns
-        -------
-        str
-            The project id.
-
-        """
-        return self._id
 
     def _check_schema_compatibility(self):
         """Check whether this project's data schema is compatible with this version.
@@ -1595,7 +1570,6 @@ class Project:
             if make_dir:
                 _mkdir_p(os.path.dirname(fn_config))
             config = read_config_file(fn_config)
-            config["project"] = name
             if workspace is not None:
                 config["workspace_dir"] = workspace
             config["schema_version"] = SCHEMA_VERSION
@@ -1603,7 +1577,6 @@ class Project:
             project = cls.get_project(root=root)
             if name is not None:
                 project.doc[name_key] = name
-            assert project.id == str(name)
             return project
         else:
             if workspace is not None and os.path.realpath(
@@ -1653,7 +1626,7 @@ class Project:
             )
 
         config = load_config(root=root, local=False)
-        if "project" not in config or (
+        if "project_dir" not in config or (
             not search
             and os.path.realpath(config["project_dir"]) != os.path.realpath(root)
         ):
