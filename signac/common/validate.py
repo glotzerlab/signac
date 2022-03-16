e659117d87c34e3e552f3c0bab2e354a2c65cbe2
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2022-03-15 21:37:28 -0700

    Move discovery to separate functions. (#711)
    * Move discovery to separate functions.
    
    * Address first round of PR comments.
    
    * Address PR comments.
    
    * Apply suggestions.

--- signac/common/validate.py
+++ signac/common/validate.py
@@ -14,6 +14,7 @@ def get_validator():  # noqa: D103
     return Validator()
 
 
+# TODO: Rename to something internal and uppercase e.g. _CFG.
 cfg = """
 workspace_dir = string(default='workspace')
 schema_version = string(default='1')
