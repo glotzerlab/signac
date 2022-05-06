863fbc51ef7352046e98261d939f94e811feaf35
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2022-05-06 12:04:55 -0700

    Remove compatibility layers and other deprecated code (#752)
    * Remove doc filter support.
    
    * Remove support for workspace as function.
    
    * Remove backwards compatibility layer for project_names in init.
    
    * Remove backwards compat layers for schema.
    
    * Remove backwards compat layers in CLI.
    
    * Get all tests passing.
    
    * Fix style.
    
    * Update changelog.
    
    * Update changelog.
    
    * Address all PR comments except for renaming init_project parameter.

--- tests/test_temporary_project.py
+++ tests/test_temporary_project.py
@@ -9,17 +9,17 @@ import signac
 class TestTemporaryProject:
     def test_init_context_manager_constructor(self):
         with signac.TemporaryProject() as tmp_project:
-            assert os.path.isdir(tmp_project.root_directory())
+            assert os.path.isdir(tmp_project.path)
             for i in range(10):
                 tmp_project.open_job(dict(a=i)).init()
             assert len(tmp_project) == 10
-        assert not os.path.isdir(tmp_project.root_directory())
+        assert not os.path.isdir(tmp_project.path)
 
     def test_init_project_method(self):
         with signac.TemporaryProject() as project:
             with project.temporary_project() as tmp_project:
-                assert os.path.isdir(tmp_project.root_directory())
+                assert os.path.isdir(tmp_project.path)
                 for i in range(10):
                     tmp_project.open_job(dict(a=i)).init()
                 assert len(tmp_project) == 10
-            assert not os.path.isdir(tmp_project.root_directory())
+            assert not os.path.isdir(tmp_project.path)
