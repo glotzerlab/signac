4d38f56b28da779b1cc055e059398074ed56292d
Bradley Dice <bdice@bradleydice.com>
2022-09-19 11:10:14 -0500

    Clarify behavior of groupby default=None. (#817)
    * Clarify behavior of default=None.
    
    * Fix description to include query namespaces
    
    * Update duplicate doc string
    
    * Add return values to other docstring
    
    * Update return type doc
    
    Co-authored-by: Corwin Kerr <cbkerr@umich.edu>
    Co-authored-by: Vyas Ramasubramani <vyas.ramasubramani@gmail.com>

--- signac/contrib/project.py
+++ signac/contrib/project.py
@@ -1082,7 +1082,10 @@ class Project:
         return iter(self.find_jobs())
 
     def groupby(self, key=None, default=None):
-        """Group jobs according to one or more state point parameters.
+        """Group jobs according to one or more state point or document parameters.
+
+        Prepend the key with 'sp.' or 'doc.' to specify the query namespace. If no prefix
+        is specified, group by state point key.
 
         This method can be called on any :class:`~signac.contrib.project.JobsCursor` such as
         the one returned by :meth:`~signac.Project.find_jobs` or by iterating over a
@@ -1117,23 +1120,26 @@ class Project:
 
         If `key` is None, jobs are grouped by id, placing one job into each group.
 
+        If `default` is None, only jobs with the `key` defined will be grouped.
+        Jobs without the `key` will be filtered out and not included in any
+        group.
+
         Parameters
         ----------
         key : str, iterable, or callable
-            The state point grouping parameter(s) passed as a string,
+            The grouping key(s) passed as a string,
             iterable of strings, or a callable that will be passed one
             argument, the job (Default value = None).
         default :
-            A default value to be used when a given state point key is not
+            A default value to be used when a given key is not
             present. The value must be sortable and is only used if not None
             (Default value = None).
 
         Returns
         -------
-        key : str
-            Grouped key.
-        group : iterable of Jobs
-            Iterable of `Job` instances matching this group key.
+        Tuple[str, Iterable[Job]]
+            A pair whose first element is the grouped key and whose second element
+            is an iterable of `Job` instances matching the key.
 
         """
         return self.find_jobs().groupby(key, default=default)
@@ -2659,7 +2665,10 @@ class JobsCursor:
             raise
 
     def groupby(self, key=None, default=None):
-        """Group jobs according to one or more state point parameters.
+        """Group jobs according to one or more state point or document parameters.
+
+        Prepend the key with 'sp.' or 'doc.' to specify the query namespace. If no prefix
+        is specified, group by state point key.
 
         This method can be called on any :class:`~signac.contrib.project.JobsCursor` such as
         the one returned by :meth:`~signac.Project.find_jobs` or by iterating over a
@@ -2694,16 +2703,26 @@ class JobsCursor:
 
         If `key` is None, jobs are grouped by id, placing one job into each group.
 
+        If `default` is None, only jobs with the `key` defined will be grouped.
+        Jobs without the `key` will be filtered out and not included in any
+        group.
+
         Parameters
         ----------
         key : str, iterable, or callable
-            The state point grouping parameter(s) passed as a string,
+            The grouping key(s) passed as a string,
             iterable of strings, or a callable that will be passed one
             argument, the job (Default value = None).
         default :
-            A default value to be used when a given state point key is not
+            A default value to be used when a given key is not
             present. The value must be sortable and is only used if not None
             (Default value = None).
+
+        Returns
+        -------
+        Tuple[str, Iterable[Job]]
+            A pair whose first element is the grouped key and whose second element
+            is an iterable of `Job` instances matching the key.
 
         """
         _filter = self._filter
