ea8455ea96492b5dc2e461db3b31118776915801
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2022-09-24 20:34:58 -0700

    Final test, make sure TestPyPI upload works.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ except OSError:
 
 setup(
     name="signac",
-    version="1.7.0",
+    version="1.7.0.dev1",
     packages=find_packages(),
     zip_safe=True,
     maintainer="signac Developers",
