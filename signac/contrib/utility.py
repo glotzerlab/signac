e1606eae298b8eed2d9bb081f45957c15eab609b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 20:49:09 +0000

    Adding numeric_owner as keyword arguement

--- signac/contrib/utility.py
+++ signac/contrib/utility.py
@@ -383,7 +383,7 @@ def _extract(filename):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
 
-                    tar.extractall(path, members, numeric_owner)
+                    tar.extractall(path, members, numeric_owner=numeric_owner)
 
                 safe_extract(file, path=tmpdir)
                 yield tmpdir
