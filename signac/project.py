ac75665c647929c91040b48f89f8c460f30ed8bb
Bradley Dice <bdice@bradleydice.com>
2023-04-23 14:06:04 -0700

    Check statepoint rather than id.

--- signac/project.py
+++ signac/project.py
@@ -520,7 +520,7 @@ class Project:
             raise ValueError("Must provide statepoint or id.")
         elif statepoint is not None and id is not None:
             raise ValueError("Either statepoint or id must be provided, but not both.")
-        elif id is None:
+        elif statepoint is not None:
             # Second best case (Job will update self._sp_cache on init)
             return Job(project=self, statepoint=statepoint)
         try:
