9ad014abb56031ab051faa96b3e374b1184ad75c
Joshua A. Anderson <joaander@umich.edu>
2024-07-23 13:00:25 -0400

    Address readthedocs deprecations.
    https://about.readthedocs.com/blog/2024/07/addons-by-default/

--- doc/conf.py
+++ doc/conf.py
@@ -11,6 +11,16 @@ to show the default.
 
 import sphinx_rtd_theme
 
+import os
+
+# Set canonical URL from the Read the Docs Domain
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+html_context = {}
+if os.environ.get("READTHEDOCS", "") == "True":
+    html_context["READTHEDOCS"] = True
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
