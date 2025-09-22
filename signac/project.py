d61227c4d9cdc62ea33062cc9bef943953b20c97
Corwin Kerr <cbkerr@umich.edu>
2025-09-22 17:08:31 -0400

    Wrap a different way for older python versions

--- signac/project.py
+++ signac/project.py
@@ -1731,8 +1731,8 @@ class Project:
         if any(is_bad_key := [a is _DictPlaceholder for a in need_to_ignore]):
             bad_keys = list(compress(ignore, is_bad_key))
             warnings.warn(
-                f"Ignored state point parameter{'s' if len(
-                bad_keys) > 1 else ''} {bad_keys} not present in project.",
+                f"Ignored state point parameter{'s' if len(bad_keys) > 1 else ''} {bad_keys}"\
+                " not present in project.",
                 RuntimeWarning,
             )
             for bad_key in bad_keys:
